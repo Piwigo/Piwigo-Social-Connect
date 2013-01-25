5ced54446750225d3dc95dbe781fce9f394df72b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-25 17:40:14 +0000

    [oauth] Add da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@20379 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/help.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['On the <b>API Access</b> tab, <b>create an OAuth 2.0 Client ID</b>'] = 'På fanebladet <b>API Access</b>, <b>create an OAuth 2.0 Client ID</b>';
+$lang['On the <b>Create Client ID</b> popup, switch to advanced settings by clicking on <b>(more options)</b>'] = 'På popup\'en <b>Create Client ID</b>, skift til avancerede indstillinger ved at klikke på <b>(more options)</b>';
+$lang['Once the application is registered update the permissions : set <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'Når applikationen er registreret, skal tilladelser opdateres: opsæt <b>Contacts</b> som <em>Read</em> og <b>Social Directory</b> som <em>Read Public</em>';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Når du har registreret dig, så kopier de oprettede applikationsloginoplysninger til opsætningssiden';
+$lang['Provide this URL as the Callback/Redirect URL for your application: <em>%s</em>'] = 'Denne URL benyttes som din applikations tilbagekalds-/viderestillings-URL (Callback/Redirect): <em>%s</em>';
+$lang['Put your website domain in the %s fields. It should match with the current hostname: <em>%s</em>'] = 'Skriv din hjemmesides domæne i %s-felterne. Det skal svare til det aktuelle værtsnavn: <em>%s</em>';
+$lang['Select <em>Website with facebook authentication</em> as application type'] = 'Vælg <em>Website with facebook authentication</em> som applikationstype';
+$lang['Set the <b>Access Scopes</b> to <em>This app will only access public...</em>'] = 'Opsæt <b>Access Scopes</b> til <em>This app will only access public...</em>';
+$lang['Set the <b>Application Type</b> to <em>Web Application</em>'] = 'Opsæt <b>Application Type</b> til <em>Web Application</em>';
+$lang['Set the <b>Default Access Type</b> to <em>Read only</em>'] = 'Opsæt <b>Default Access Type</b> til <em>Read only</em>';
+$lang['Set the <b>Kind of Application</b> to <em>Web-based</em>'] = 'Opsæt <b>Kind of Application</b> til <em>Web-based</em>';
+$lang['Fill out any required fields such as the application name and description'] = 'Udfyld krævede felter så som applikationsnavn og beskrivelse';
+$lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Gå til <a href="%s" target="_blank">%s</a> og opret en ny applikation';
+$lang['No registration required for OpenID based providers'] = 'Registrering ikke krævet ved OpenID-baserede leverandører';
+?>
---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Logged with'] = 'Logget på med';
+$lang['Providers'] = 'Leverandører';
+$lang['Sign in with %s'] = 'Log på med %s';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Nye registreringer er desværre slået fra i dette galleri.';
+$lang['Users are not allowed to register on your gallery. OAuth will not work correctly.'] = 'Brugere har ikke mulighed for at registrere sig i dit galleri. OAuth vil ikke fungere korrekt.';
+$lang['You registered with a %s account, please sign in with the same account.'] = 'Du registrerede dig med en %s-konto, vær venlig at benytte den samme konto.';
+$lang['Your registration is almost done, please complete the registration form.'] = 'Din registrering er næsten gennemført, vær venlig at udfylde registreringsformularen.';
+$lang['%s: invalid keys'] = '%s: ugyldige nøgler';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'En fejl opstod, kontakt galleriets ejer. <i>Fejlkode: %s</i>';
+$lang['Authentication canceled'] = 'Autentifikation afbrudt';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'Kontakter <b>%s</b>. Vent lidt.';
+$lang['Disabled'] = 'Deaktiveret';
+$lang['Display sign in buttons in the menubar'] = 'Vis logonknapper i menubjælken';
+$lang['Display sign in buttons on the register page'] = 'Vis logonknapper på loginsiden';
+$lang['Enabled'] = 'Aktiveret';
+$lang['Error...'] = 'Fejl ...';
+$lang['Icon size in the menubar'] = 'Ikonstørrelse i menubjælken';
+$lang['Icon size on the identification page'] = 'Ikonstørrelse på loginsiden';
+$lang['Loading...'] = 'Indlæser ...';
+$lang['OAuth: PHP Curl extension is needed'] = 'OAuth: PHP Curl-udvidelse er nødvendig';
+$lang['OAuth: You need to configure the credentials'] = 'OAuth: Du skal opsætte loginoplysninger';
+$lang['OpenID must be enabled in order to use %s authentication'] = 'OpenID skal være aktiveret for at kunne benytte %s-autentificering';
+$lang['Or sign in with'] = 'Eller log på med';
+$lang['Please enter your OpenID URL'] = 'Skriv din OpenID-URL';
+$lang['Please enter your username'] = 'Skriv dit brugernavn';
+$lang['Profile URL'] = 'Profil-URL';
+?>
