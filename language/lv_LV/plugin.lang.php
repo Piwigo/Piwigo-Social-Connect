480dc22ded11db0d32925337b6721da3e39bbdd3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-27 19:01:11 +0000

    [oauth] Add lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@20427 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['OpenID must be enabled in order to use %s authentication'] = 'Lai lietotu %s autentifikāciju, jābūt iespējotam OpenID ';
+$lang['Your registration is almost done, please complete the registration form.'] = 'Jūsu reģistrācija ir gabdrīz pabeigta, lūdzu pabeidziet reģistrācijas formu.';
+$lang['You registered with a %s account, please sign in with the same account.'] = 'Jūs esat reģistrējies ar %s kontu, lūdzu piesakieties ar šo pašu kontu.';
+$lang['Users are not allowed to register on your gallery. OAuth will not work correctly.'] = 'Lietotājiem nav atļauts reģistrēties jūsu galerijā. OAtuh nedarbosies korekti.';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Atvainojiet, šai galerijā jauna reģistrēšanās bloķēta';
+$lang['OAuth: You need to configure the credentials'] = 'OAuth: Nepieciešams nokonfigurēt Jūsu akreditēšanās datus';
+$lang['OAuth: PHP Curl extension is needed'] = 'OAuth: nepieciešams PHP Curl paplašinājums';
+$lang['Please enter your OpenID URL'] = 'Lūdzu ievadied savu OpenId URL';
+$lang['Please enter your username'] = 'Lūdzu ievadiet savu lietotājvārdu';
+$lang['Sign in with %s'] = 'Piesakies ar %s';
+$lang['Providers'] = 'Pakalpojuma sniedzēji';
+$lang['Icon size in the menubar'] = 'Ikonas izmērs izvēlnes joslā';
+$lang['Profile URL'] = 'URL Profils';
+$lang['Or sign in with'] = 'Vai piesakies ar ';
+$lang['Logged with'] = 'Ielogojies ar';
+$lang['Icon size on the identification page'] = 'Ikonas izmērs iedentifikācijas lapā';
+$lang['Loading...'] = 'Ielādē....';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Ir radusies kļūda, lūdzu sazinieties ar galerijas īpašnieku.<i>Kļūdas kods : %s</i>';
+$lang['Display sign in buttons in the menubar'] = 'Attēlot zīmi izvēlnes joslas pogās';
+$lang['Display sign in buttons on the register page'] = 'Attēlot zīmi reģistrācijas lapas pogās';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'Savienojas <b>%s</b>. Lūdzu uzgaidiet.';
+$lang['Authentication canceled'] = 'Autentifikācija atcelta';
+$lang['%s: invalid keys'] = '%s: kļūdainas atslēgas';
+$lang['Error...'] = 'Kļūda...';
+$lang['Enabled'] = 'Iespējots';
+$lang['Disabled'] = 'Atspējots';
+?>
