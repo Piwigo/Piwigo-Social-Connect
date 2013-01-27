840173076954f9ae1e29c544b5cee7f04513359e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-27 09:54:01 +0000

    [oauth] Add es_ES, thanks to : jpr928
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@20414 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/es_ES/help.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['On the <b>Create Client ID</b> popup, switch to advanced settings by clicking on <b>(more options)</b>'] = 'En la popup <b>Create Client ID</b>, hagan clic en <b>(more options)</b>';
+$lang['Once the application is registered update the permissions : set <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'Cuando la aplicación está registrada, editar los permisos: seleccione <em>Read</em> para <b>Contacts</b> y <em>Read Public</em> para <b>Social Directory</b>';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Cuando su solicitud sea registrada, copie y pegue las claves de autentificación en esta página';
+$lang['Provide this URL as the Callback/Redirect URL for your application: <em>%s</em>'] = 'Dar esta URL como  Callback/Redirect URL para su aplicación';
+$lang['Put your website domain in the %s fields. It should match with the current hostname: <em>%s</em>'] = 'Ponga su nombre de dominio en los campos %s. Debe corresponder a <em>%s</em>';
+$lang['Select <em>Website with facebook authentication</em> as application type'] = 'Elige <em>Sitio web con autenticación facebook </em> como tipo de aplicación';
+$lang['Set the <b>Access Scopes</b> to <em>This app will only access public...</em>'] = 'Seleccionar <em>This app will only access public...</em> para <b>Access Scopes</b>';
+$lang['Set the <b>Application Type</b> to <em>Web Application</em>'] = 'Seleccionar <em>Web Application</em> para <b>Application Type</b>';
+$lang['Set the <b>Default Access Type</b> to <em>Read only</em>'] = 'Seleccionar <em>Read only</em> para <b>Default Access Type</b>';
+$lang['Set the <b>Kind of Application</b> to <em>Web-based</em>'] = 'Seleccionar <em>Web-based</em> para <b>Kind of Application</b>';
+$lang['On the <b>API Access</b> tab, <b>create an OAuth 2.0 Client ID</b>'] = 'En la pestaña <b>API Access</b>, hagan clic en <b>create an OAuth 2.0 Client ID</b>';
+$lang['No registration required for OpenID based providers'] = 'No es necesario registrarse para tipos de autenticación OpenID';
+$lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Vallan a <a href="%s" target="_blank">%s</a> y crean una nueva aplicación';
+$lang['Fill out any required fields such as the application name and description'] = 'Rellene todos los campos necesarios como el nombre y la descripción de la aplicación';
+?>
---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Please enter your username'] = 'Por favor, introduzca su nombre de usuario';
+$lang['Profile URL'] = 'URL del perfil';
+$lang['Providers'] = 'Proveedores';
+$lang['Sign in with %s'] = 'Conexión con %s';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Lo sentimos, los registros están bloqueados en esta galería.';
+$lang['Users are not allowed to register on your gallery. OAuth will not work correctly.'] = 'Los usuarios no están autorizados a registrarse en esta galería. OAuth no funcionará correctamente.';
+$lang['You registered with a %s account, please sign in with the same account.'] = 'Se ha registrado una cuenta %s. Por favor, inicie sesión con la misma cuenta.
+';
+$lang['Your registration is almost done, please complete the registration form.'] = 'Su registro está casi completo, gracias por completar el formulario de inscripción.';
+$lang['Logged with'] = 'Conectado con';
+$lang['OAuth: PHP Curl extension is needed'] = 'OAuth: La extensión PHP Curl es necesaria';
+$lang['OAuth: You need to configure the credentials'] = 'OAuth: Debe configurar la clave de autenticación';
+$lang['OpenID must be enabled in order to use %s authentication'] = 'OpenID debe estar activada para utilizar la autenticación %s';
+$lang['Or sign in with'] = 'O conectase con';
+$lang['Please enter your OpenID URL'] = 'Por favor, introduzca su URL OpenID';
+$lang['%s: invalid keys'] = '%s: Claves inválidas';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Ha ocurrido un error, por favor póngase en contacto con el propietario de la galería. <i> código de error: %s</i>';
+$lang['Authentication canceled'] = 'Autentificación cancelada';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'Contactando <b>%s</b>.Espere';
+$lang['Disabled'] = 'Desactivado';
+$lang['Display sign in buttons in the menubar'] = 'Mostrar señales en los botones de la barra de menú';
+$lang['Display sign in buttons on the register page'] = 'Mostrar señales en los botones de la pagina de registro';
+$lang['Enabled'] = 'Habilitado';
+$lang['Error...'] = 'Error...';
+$lang['Icon size in the menubar'] = ' Tamaño de icono en la barra de menú';
+$lang['Loading...'] = 'Cargando...';
+$lang['Icon size on the identification page'] = ' Tamaño de icono en la pagina de identificación';
+?>
