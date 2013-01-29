3a54bc1c3a4b2cc7560c235e34173f09730f33e7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-29 10:53:11 +0000

    [oauth] Add el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@20446 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Logged with'] = 'Καταγράφηκε με';
+$lang['Loading...'] = 'Φόρτωση ...';
+$lang['Icon size on the identification page'] = 'Μέγεθος εικονιδίων στην σελίδα αναγνώρισης';
+$lang['Icon size in the menubar'] = 'Μέγεθος Εικονίδιου στη γραμμή μενού';
+$lang['Error...'] = 'Σφάλμα .. ';
+$lang['Enabled'] = 'Ενεργοποιημένο';
+$lang['Display sign in buttons on the register page'] = 'Εμφάνιση σφραγίδας στα κουμπιά στη σελίδα εγγραφής';
+$lang['Display sign in buttons in the menubar'] = 'Εμφάνιση σφραγίδας στα κουμπιά στη γραμμή μενού';
+$lang['Disabled'] = 'Απενεργοποιημένο';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'Επικοινωνία με <b>%s</b>. Παρακαλώ περιμένετε.';
+$lang['Authentication canceled'] = 'Η Ταυτοποίηση ακυρώθηκε';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Παρουσιάστηκε σφάλμα, παρακαλούμε επικοινωνήστε με τον ιδιοκτήτη της γκαλερί. <i>Κωδικός σφάλματος: %s</i>';
+$lang['%s: invalid keys'] = '%s: μη έγκυρα κλειδιά';
+?>
