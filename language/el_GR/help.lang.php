734e35f911ad6248dcb10dac2fc49f9fb1db778b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-31 13:10:02 +0000

    [oauth] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@20502 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/el_GR/help.lang.php
+++ language/el_GR/help.lang.php
@@ -29,9 +29,9 @@ $lang['Put your website domain in the %s fields. It should match with the curren
 $lang['Provide this URL as the Callback/Redirect URL for your application: <em>%s</em>'] = 'Δώστε αυτό το URL ως το Callback/Redirect URL για την εφαρμογή σας: <em>%s</em>';
 $lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Μόλις κάνετε την εγγραφή, κάντε αντιγραφή και επικόλληση τα διαπιστευτήρια της εφαρμογής που δημιουργήσατε σε αυτή τη σελίδα ρύθμισης';
 $lang['Once the application is registered update the permissions : set <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'Μόλις η εφαρμογή καταχωρείται ενημερώσετε τα δικαιώματα : βάλτε <b>Επαφές</b> ως <em>Read</em> και <b>Social Directory</b> ως <em>Read Public</em>';
-$lang['On the <b>Create Client ID</b> popup, switch to advanced settings by clicking on <b>(more options)</b>'] = ' αναδυόμενο παράθυρο <b>Δημιουργία ID Πελάτη</b>, μεταβείτε στις ρυθμίσεις για προχωρημένους κάνοντας κλικ στο <b>(περισσότερες επιλογές)</b>';
+$lang['On the <b>Create Client ID</b> popup, switch to advanced settings by clicking on <b>(more options)</b>'] = 'Στο αναδυόμενο παράθυρο <b>Δημιουργία ID Πελάτη</b>, μεταβείτε στις ρυθμίσεις για προχωρημένους κάνοντας κλικ στο <b>(περισσότερες επιλογές)</b>';
 $lang['On the <b>API Access</b> tab, <b>create an OAuth 2.0 Client ID</b>'] = 'Από την <b>API Access</b>  καρτέλα, <b>δημιουργήστε ένα OAuth 2.0 Client ID</b>';
-$lang['No registration required for OpenID based providers'] = 'Δεν απαιτείται εγγραφή για OpenID με βάση τους παρόχους';
+$lang['No registration required for OpenID based providers'] = 'Δεν απαιτείται εγγραφή για OpenID με βάση παρόχους';
 $lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Μετάβαση στο <a href="%s" target="_blank">%s</a> και δημιουργήστε μια νέα εφαρμογή';
 $lang['Fill out any required fields such as the application name and description'] = 'Συμπληρώστε όλα τα απαιτούμενα πεδία, όπως το όνομα της εφαρμογής και περιγραφή';
 ?>
