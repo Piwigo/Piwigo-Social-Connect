80c2fda9d4e8642e14fbe6a5faca4d72f2db72cb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-03 17:11:10 +0000

    [oauth] Update lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@20532 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/help.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Once the application is registered update the permissions : set <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'Ja aplikācija ir reģistrēta atjauniniet atļaujas: iestatiet <b>Kontaktus</b> kā <em>Lasāms</em> un <b>Sociālo Mapi</b> kā <em>Publiski Lasāms</em>';
+$lang['Fill out any required fields such as the application name and description'] = 'Aizpildiet visus pieprasītos laukus, kā aplikācijas nosaukums un apraksts';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Ja reiz esat piereģistrēti, nokopējiet un ielīmējiet izveidotās aplikācijas reģistrācijas datus šajā uzstādīšanas (setup) lapā';
+$lang['Provide this URL as the Callback/Redirect URL for your application: <em>%s</em>'] = 'Piestādiet šo URL kā jūsu aplikācijas Atbildes/Pāradresācijas URL: <em>%s</em>';
+$lang['Put your website domain in the %s fields. It should match with the current hostname: <em>%s</em>'] = 'Ievietojiet savu vietnes domēnu %s laukos. Tam jāsakrīt ar esošo hosta nosaukumu: <em>%s</em>';
+$lang['Select <em>Website with facebook authentication</em> as application type'] = 'Izvēlaties <em>Interneta vietni ar facebook autentifikāciju</em> kā aplikācijas tipu';
+$lang['Set the <b>Access Scopes</b> to <em>This app will only access public...</em>'] = 'Iestatiet <b>Piekļuves apjomi</b> stāvoklī <em>Ar šo aplikāciju varēs piekļūt tikai publiskiem...</em>';
+$lang['Set the <b>Application Type</b> to <em>Web Application</em>'] = 'Iestatiet <b>Applikācijas Tipu</b> stāvoklī <em>Web Aplikācija</em>';
+$lang['Set the <b>Default Access Type</b> to <em>Read only</em>'] = 'Iestatiet<b>Piekļuves Tipu pēc noklusēšanas</b> stāvoklī <em>Tikai lasāms</em>';
+$lang['Set the <b>Kind of Application</b> to <em>Web-based</em>'] = 'Iestatiet<b>Aplikāciju veidu</b> uz <em>Web atbalstītu</em>';
+$lang['On the <b>Create Client ID</b> popup, switch to advanced settings by clicking on <b>(more options)</b>'] = '<b>Izveidot Client ID</b> uzlecošā logā, klikšķinot uz <b>(vairāk opciju)</b>, pārslēdzaties uz papildus iestatījumiem';
+$lang['On the <b>API Access</b> tab, <b>create an OAuth 2.0 Client ID</b>'] = 'Ar <b>API Access</b> tabu, <b>izveidojiet OAuth 2.0 Klienta ID</b>';
+$lang['No registration required for OpenID based providers'] = 'Uz OpenID balstītiem pakalpojuma sniedzējiem nav nepieciešama reģistrācija';
+$lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Ejiet uz <a href="%s" target="_blank">%s</a> uz izveidojiet jaunu aplikāciju';
+?>
