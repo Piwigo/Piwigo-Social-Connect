ede8f9751d757e6a2a906c805f12b76978acec68
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-04 05:37:59 +0000

    [oauth] Add tr_TR, thanks to : hakanselcuk
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@20540 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/help.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['On the <b>API Access</b> tab, <b>create an OAuth 2.0 Client ID</b>'] = '<b>OAuth 2.0 Client ID</b> sini <b>API Access</b> bölümünden oluşturun';
+$lang['Put your website domain in the %s fields. It should match with the current hostname: <em>%s</em>'] = 'Web site adresinizi %s yazınız. Geçerli alan adı ile eşleşmesi gerekir. <em>%s</em>';
+$lang['Set the <b>Kind of Application</b> to <em>Web-based</em>'] = '<b>Uygulama çeşidi<b> olarak <em>Web Tabanlı</em> seçiniz.';
+$lang['Set the <b>Access Scopes</b> to <em>This app will only access public...</em>'] = '<b> Erişim Kapsamı</b> olarak <em> Bu uygulama sadece hakla açık ...</em> seçiniz.';
+$lang['Set the <b>Application Type</b> to <em>Web Application</em>'] = '<b>Uygulama Tipi</b> olarak <em>Web Uygulaması</em> seçiniz.';
+$lang['Set the <b>Default Access Type</b> to <em>Read only</em>'] = '<b>Varsayılan Erişim Türü</b> olarak <em>Salt Okunur</em> ayarlayın';
+$lang['Select <em>Website with facebook authentication</em> as application type'] = 'Uygulama türü olarak <em> Facebook Kimlik Doğrulama</em> \'yı seçin';
+$lang['No registration required for OpenID based providers'] = 'OpenID tabanlı sağlayıcılar için kayıt olmaya gerek yok';
+$lang['Fill out any required fields such as the application name and description'] = 'Uygulama adı ve açıklaması için tüm gerekli alanları doldurun';
+$lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Yeni uygulama oluşturmak için <a href="%s" target="_blank">%s</a> tıklayın';
+?>
---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['OpenID must be enabled in order to use %s authentication'] = '%s Kimlik Doğrulamasını kullanmak için OpenID etkin olmalıdır.';
+$lang['Icon size on the identification page'] = 'Tanımlama sayfasında simge boyutu';
+$lang['Icon size in the menubar'] = 'Menüde simge boyutu.';
+$lang['You registered with a %s account, please sign in with the same account.'] = 'Kayıt olduğunuz hesap %s , lütfen aynı hesap ile oturum açın.';
+$lang['Sign in with %s'] = '%s İle Giriş yapın';
+$lang['Your registration is almost done, please complete the registration form.'] = 'Kaydınızın tamamlanması için, Lütfen Kayıt formunu doldurun';
+$lang['OAuth: You need to configure the credentials'] = 'OAuth: Kimlik bilgilerinizi yapılandırmanız gerekli';
+$lang['Users are not allowed to register on your gallery. OAuth will not work correctly.'] = 'Kullanıcılar galerinize kayıt olma yetkisine sahip değiller. OAuth ayarları doğru çalışmıyor.';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Üzgüzün, bu galeri için yeni kayıtlar engelli';
+$lang['OAuth: PHP Curl extension is needed'] = 'OAuth: PHP Curl eklentisi gerekli';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'Ulaşılıyor <b>%s</b>. Lütfen Bekleyin.';
+$lang['Disabled'] = 'Devre Dışı';
+$lang['Enabled'] = 'Devrede';
+$lang['Logged with'] = 'ile giriş yapıldı';
+$lang['Providers'] = 'Sağlayıcılar';
+$lang['Display sign in buttons on the register page'] = 'Kayıt Sayfasında Sign in Butonunu gösterin';
+$lang['Display sign in buttons in the menubar'] = 'Menüde Sign in butonunu gösterin';
+$lang['%s: invalid keys'] = 'Geçersiz anahtar';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Bir hata oluştu. Lütfen Galeri sahibi ile irtibata geçiniz. <i>Hata Kodu : %s </iS';
+$lang['Authentication canceled'] = 'Doğrulama iptal edildi.';
+$lang['Error...'] = 'Hata...';
+$lang['Loading...'] = 'Yükleniyor...';
+$lang['Please enter your username'] = 'Lütfen Kullanıcı adınızı giriniz.';
+$lang['Profile URL'] = 'Profil URL';
+$lang['Please enter your OpenID URL'] = 'Lütfen OpenID URL nizi giriniz.';
+$lang['Or sign in with'] = 'ile giriş yapın';
+?>
