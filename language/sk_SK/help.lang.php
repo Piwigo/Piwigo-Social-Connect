44093546920418f5c03674d8c60240c7b60dafbf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-04 18:20:52 +0000

    [oauth] Add sk_SK, thanks to : dodo
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@20552 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/help.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Set the <b>Kind of Application</b> to <em>Web-based</em>'] = 'Nastaviť <b>Kind of Application</b> do <em>Web-based</em>';
+$lang['Set the <b>Default Access Type</b> to <em>Read only</em>'] = 'Nastaviť <b>Default Access Type</b> na <em>Read only</em>';
+$lang['Set the <b>Application Type</b> to <em>Web Application</em>'] = 'Nastaviť <b>Application Type</b> do <em>Web aplikácie</em>';
+$lang['Set the <b>Access Scopes</b> to <em>This app will only access public...</em>'] = 'Nastaviť <b>Access Scopes</b> do <em>tejto app bude len verejne prístupná...</em>';
+$lang['Select <em>Website with facebook authentication</em> as application type'] = 'Označiť <em>Website with facebook authentication</em> ako typ aplikácie';
+$lang['Put your website domain in the %s fields. It should match with the current hostname: <em>%s</em>'] = 'Dať Vašu doménu webstránky do poľa %s';
+$lang['Provide this URL as the Callback/Redirect URL for your application: <em>%s</em>'] = 'Poskytnutie tejto URL ako Callback/Redirect URL pre Vašu aplikáciu: <em>%s</em>';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Akonáhle ste registrovaný, prekopírujte vytvorenú aplikáciu poverení na stránku inštalácie';
+$lang['Once the application is registered update the permissions : set <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'Akonáhle aplikácia zaregistruje aktualizáciu prístupov : nastavte <b>Contacts</b> ako <em>Read</em> a <b>Social Directory</b> ako <em>Read Public</em>';
+$lang['On the <b>Create Client ID</b> popup, switch to advanced settings by clicking on <b>(more options)</b>'] = 'Na <b>Create Client ID</b>, prepmite na doplňujúce nastavenia kliknutím na <b>(more options)</b>';
+$lang['On the <b>API Access</b> tab, <b>create an OAuth 2.0 Client ID</b>'] = 'Na <b>API Access</b> záložke, <b>vytvorenie OAuth 2.0 ID klienta</b>';
+$lang['No registration required for OpenID based providers'] = 'Nie je potrebná registrácia pre OpenID základných poskytovateľov';
+$lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Choďte na <a href="%s" target="_blank">%s</a> a vytvorte novú aplikáciu';
+$lang['Fill out any required fields such as the application name and description'] = 'Rozšíriť požadované údaje ako názov aplikácie a popis';
+?>
---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Your registration is almost done, please complete the registration form.'] = 'Vaša registrácia bola vykonaná, prosím skompletizujte registračný formulár.';
+$lang['You registered with a %s account, please sign in with the same account.'] = 'Boli ste zaregistrovaný s %s kontom, prosím zapíšte sa rovnakým kontom.';
+$lang['Users are not allowed to register on your gallery. OAuth will not work correctly.'] = 'Používatelia nie sú povolení pre registráciu do tejto galérie. OAuth nebude pracovať správne.';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Prepáčte, nové registrácie sú zakázané pre túto galériu.';
+$lang['Sign in with %s'] = 'Podpísane s %s';
+$lang['Providers'] = 'Poskytovatelia';
+$lang['Profile URL'] = 'Profil URL';
+$lang['Please enter your username'] = 'Prosím vložte Vaše používateľské menu';
+$lang['Please enter your OpenID URL'] = 'Prosím vložte Vašu OpenID URL';
+$lang['Or sign in with'] = 'Alebo podpíšte s';
+$lang['OpenID must be enabled in order to use %s authentication'] = 'OpenID musí byť povolené v %s autorizácii';
+$lang['OAuth: You need to configure the credentials'] = 'OAuth: Potrebujete nakonfigurovať poverenia';
+$lang['Display sign in buttons on the register page'] = 'Zobraziť podpisovacie tlačidlo na registračnej stránke';
+$lang['Display sign in buttons in the menubar'] = 'Zobraziť podpisovacie tlačidlo v stĺpci menu';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Nastala chyba, prosím kontaktujte majiteľa galérie. <i>Kód chyby: %s</i>';
+$lang['OAuth: PHP Curl extension is needed'] = 'OAuth: je potrebný doplnok PHP Curl';
+$lang['Logged with'] = 'Spojené s';
+$lang['Loading...'] = 'Nahrávam...';
+$lang['Icon size on the identification page'] = 'Veľkosť ikony na identifikačnej stránke';
+$lang['Icon size in the menubar'] = 'Veľkosť ikony v stĺpci menu';
+$lang['Error...'] = 'Chyba...';
+$lang['Enabled'] = 'Povolené';
+$lang['Disabled'] = 'Zakázané';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'Spájam <b>%s</b>. Prosím čakajte.';
+$lang['Authentication canceled'] = 'Autorizácia zrušená';
+$lang['%s: invalid keys'] = '%s: neplatné kľúče';
+?>
