9c4956a6e260b3db8d5e11479964f0adea2235d3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-04 18:19:42 +0000

    [oauth] Add it_IT, thanks to : lucser
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@20548 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Contacting <b>%s</b>. Please wait.'] = 'Sto contattando <b>%s</b>. Un attimo prego.';
+$lang['Profile URL'] = 'URL del profilo';
+$lang['%s: invalid keys'] = '%s: indice invalido';
+$lang['Authentication canceled'] = 'Autenticazione cancellata';
+$lang['Disabled'] = 'Disabilitato';
+$lang['Enabled'] = 'Abilitato';
+$lang['Error...'] = 'Errore...';
+?>
