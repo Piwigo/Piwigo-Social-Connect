6a6902c25118fc55914ac146fd06fcf9841557c7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-09 22:50:56 +0000

    [oauth] Update it_IT, thanks to : Ericnet
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@20631 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/it_IT/help.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['On the <b>API Access</b> tab, <b>create an OAuth 2.0 Client ID</b>'] = 'Sulla scheda <b>API Access</b>, <b>creare un OAuth 2.0 Client ID</b>';
+$lang['Once the application is registered update the permissions : set <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'Una volta che l\'applicazione è registrata aggiornare le autorizzazioni: impostare <b>Contacts</b> come <em>Read</em> e <b>Social Directory</b> come <em>Read Public</em>';
+$lang['Set the <b>%s</b> to <em>%s</em>'] = 'Impostare il <b>%s</b> per <em>%s</em>';
+$lang['Put your website domain in the %s fields. It should match with the current hostname: <em>%s</em>'] = 'Metti il ​​tuo dominio del sito web nei campi del %s. Esso deve corrispondere con l\'attuale hostname: <em>%s</em>';
+$lang['Provide this URL as the Callback/Redirect URL for your application: <em>%s</em>'] = 'Fornisci questo URL come Callback/Redirect URL per l\'applicazione:
+<em>%s</em>';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Una volta registrato, copiare e incollare le credenziali delle applicazioni create in questa pagina di configurazione';
+$lang['On the <b>Create Client ID</b> popup, switch to advanced settings by clicking on <b>(more options)</b>'] = 'Sulla finestra popup <b>Create Client ID</b>, passare alle impostazioni avanzate, fare clic su <b>(more options)</ b>';
+$lang['No registration required for OpenID based providers'] = 'Non è richiesta la registrazione per i providers OpenID base';
+$lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Vai a <a href="%s" target="_blank">%s</a> e crea una nuova applicazione';
+$lang['Fill out any required fields such as the application name and description'] = 'Riempire tutti i campi obbligatori, quali il nome e la descrizione';
+?>
