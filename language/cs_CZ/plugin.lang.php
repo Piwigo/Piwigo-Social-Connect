a0a6f9b042bcf39f8403ebfe0f72e314c867b227
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-23 07:04:55 +0000

    [oauth] Update cs_CZ, thanks to : sichr
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@20981 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Providers'] = 'Poskytovatelé';
+$lang['Sign in with %s'] = 'Přihlásit se pomocí %s';
+$lang['Authentication canceled'] = 'Ověření zrušeno';
+$lang['Disabled'] = 'Zrušeno';
+$lang['Enabled'] = 'Povoleno';
+$lang['Error...'] = 'Chyba...';
+$lang['Icon size in the menubar'] = 'Velikost ikony v menu';
+$lang['Loading...'] = 'Nahrávání...';
+$lang['Logged with'] = 'Přihlášen pomocí';
+$lang['Or sign in with'] = 'Nebo se přihlaste pomocí';
+$lang['Please enter your OpenID URL'] = 'Zadejte prosím vaše OpenID URL';
+$lang['Please enter your username'] = 'Zadejte prosím vaše uživatelské jméno';
+$lang['Profile URL'] = 'URL profilu';
+?>
