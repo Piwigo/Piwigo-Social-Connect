6d0d5a6ca315dac64cfa88c8d1d0b45bc57b9030
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-24 06:26:07 +0000

    [oauth] Add pt_PT, thanks to : toze
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@20997 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_PT/help.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Fill out any required fields such as the application name and description'] = 'Preencha os campos obrigatórios como o nome da aplicação e descrição';
+$lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Ir para <a href="%s" target="_blank">%s</a> e criar uma nova aplicação';
+$lang['No registration required for OpenID based providers'] = 'Registo não necessário para provedores baseados em OpenID ';
+$lang['On the <b>API Access</b> tab, <b>create an OAuth 2.0 Client ID</b>'] = 'No separador <b>Acesso API</b>, <b>crie um Client ID OAuth 2.0</b>';
+$lang['On the <b>Create Client ID</b> popup, switch to advanced settings by clicking on <b>(more options)</b>'] = 'No popup <b>Criar ID Cliente</b>, mude para definições avançadas clicando no <b>(mais opções)</b>';
+$lang['Once the application is registered update the permissions : set <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'Assim que aplicação esteja registada actualize as permissões: defina <b>Contactos</b> como <em>Leitura</em> e <b>Directoria Social</b> como <em>Leitura Pública</em>';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Uma vez registado, copie e cole as credenciais criadas pela aplicação nesta página de instalação.';
+$lang['Provide this URL as the Callback/Redirect URL for your application: <em>%s</em>'] = 'Forneça este URL como o URL Callback/Redirect para a sua aplicação: <em>%s</em>';
+$lang['Put your website domain in the %s fields. It should match with the current hostname: <em>%s</em>'] = 'Coloque o domínio do seu website no campos %s. Deverá ser igual ao seu anfitrião actual:<em>%s</em>';
+$lang['Set the <b>%s</b> to <em>%s</em>'] = 'Altere o <b>%s</b> para <em>%s</em>';
+?>
---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Providers'] = 'Provedores';
+$lang['Sign in with %s'] = 'Entre com %s';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Desculpe, novos registos estão bloqueados nesta galeria.';
+$lang['Users are not allowed to register on your gallery. OAuth will not work correctly.'] = 'Não permitido aos utilizadores registarem-se na sua galeria. OAuth não vai funcionar correctamente.';
+$lang['You registered with a %s account, please sign in with the same account.'] = 'Registou-se com uma conta %s, por favor entre com a mesma conta.';
+$lang['Your registration is almost done, please complete the registration form.'] = 'O seu registo está quase completo, por favor termine o formulário de registo.';
+$lang['%s: invalid keys'] = '%s: chaves inválidas';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Ocorreu um erro, por favor contacte o dono da galeria. <i>Código do erro: %s</i>';
+$lang['Authentication canceled'] = 'Autenticação cancelada';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'Contactando <b>%s</b>. Por favor aguarde.';
+$lang['Disabled'] = 'Deactivado';
+$lang['Display sign in buttons in the menubar'] = 'Mostrar botões de sign in na barra de menu';
+$lang['Display sign in buttons on the register page'] = 'Mostrar botões de sign in na página de registo';
+$lang['Enabled'] = 'Activo';
+$lang['Error...'] = 'Erro...';
+$lang['Icon size in the menubar'] = 'Tamanho do ícone na barra de menu ';
+$lang['Icon size on the identification page'] = 'Tamanho do ícone na página de identificação';
+$lang['Loading...'] = 'A carregar...';
+$lang['Logged with'] = 'Entrou com';
+$lang['OAuth: PHP Curl extension is needed'] = 'OAuth: Extensão PHP Curl é necessária';
+$lang['OAuth: You need to configure the credentials'] = 'OAuth: Precisa de configurar as credenciais ';
+$lang['OpenID must be enabled in order to use %s authentication'] = 'OpenID deve estar activo de forma a usar %s autenticação';
+$lang['Or sign in with'] = 'Ou entre com';
+$lang['Please enter your OpenID URL'] = 'Por favor insira o seu URL OpenID';
+$lang['Please enter your username'] = 'Por favor insira o seu nome de utilizador';
+$lang['Profile URL'] = 'URL do perfil';
+?>
