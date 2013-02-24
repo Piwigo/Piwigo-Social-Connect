f7133fe1fe58a907631b47002a0df6f26ad4a329
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-24 06:26:20 +0000

    [oauth] Add zh_CN, thanks to : winson
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@20998 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Display sign in buttons in the menubar'] = '在菜单栏显示登录按钮';
+$lang['Display sign in buttons on the register page'] = '在注册页面显示登录按钮';
+$lang['Enabled'] = '开启';
+$lang['Error...'] = '错误...';
+$lang['Icon size in the menubar'] = '菜单栏上的图标大小';
+$lang['Icon size on the identification page'] = '认证页面的图标大小';
+$lang['Loading...'] = '加载中...';
+$lang['Logged with'] = '登录为';
+$lang['OAuth: PHP Curl extension is needed'] = 'OAuth:需安装 PHP Curl 扩展';
+$lang['OAuth: You need to configure the credentials'] = 'OAuth:你需要配置证书';
+$lang['OpenID must be enabled in order to use %s authentication'] = '必须开启OpenID以使用 %s 认证';
+$lang['Or sign in with'] = '或者登录为';
+$lang['Please enter your OpenID URL'] = '请输入你的 OpenID 地址';
+$lang['Please enter your username'] = '请输入你的用户名';
+$lang['Profile URL'] = '个人资料地址';
+$lang['Providers'] = '提供者';
+$lang['Sign in with %s'] = '注册为 %s';
+$lang['Sorry, new registrations are blocked on this gallery.'] = '抱歉，此相册已禁止新注册';
+$lang['Users are not allowed to register on your gallery. OAuth will not work correctly.'] = '不允许用户在你的相册上注册。OAuth不能正常工作。';
+$lang['You registered with a %s account, please sign in with the same account.'] = '你注册了一个%s帐户，请使用同一个帐户登录';
+$lang['Your registration is almost done, please complete the registration form.'] = '你的注册就快完成了，请填妥以下注册表格';
+$lang['%s: invalid keys'] = '%s:无效密钥';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = '出现错误，请联系相册拥有者：<i>错误代码：%s</i>';
+$lang['Authentication canceled'] = '取消认证';
+$lang['Contacting <b>%s</b>. Please wait.'] = '正在连接<b>%s</b>，请稍候。';
+$lang['Disabled'] = '不可用';
+?>
