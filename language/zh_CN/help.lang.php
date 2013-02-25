a776fa5b4e581e4ba4931cb713c35f746c5ba8a8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-25 10:15:46 +0000

    [oauth] Update zh_CN, thanks to : foddyy
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@21021 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/help.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Fill out any required fields such as the application name and description'] = '填写例如“应用名称”、“描述”等必填字段';
+$lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = '前往<a href="%s" target="_blank">%s</a>创建一个新应用';
+$lang['No registration required for OpenID based providers'] = '支持OpenID的服务提供商无需注册';
+$lang['On the <b>API Access</b> tab, <b>create an OAuth 2.0 Client ID</b>'] = '在<b>API Access</b>标签上, <b>创建一个OAuth 2.0客户端ID</b>';
+$lang['On the <b>Create Client ID</b> popup, switch to advanced settings by clicking on <b>(more options)</b>'] = '在<b>Create Client ID</b>弹出窗口中, 点击<b>(more options)</b>切换到高级设置。';
+$lang['Once the application is registered update the permissions : set <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = '应用已注册和更新权限后，设置<b>Contacts</b>为<em>Read</em>、<b>Social Directory</b>为<em>Read Public</em>。';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = '完成注册后，复制并粘贴已创建应用的证书到安装页面。';
+$lang['Provide this URL as the Callback/Redirect URL for your application: <em>%s</em>'] = '将此链接作为你的应用的反馈/重定向链接：<em>%s</em>';
+$lang['Put your website domain in the %s fields. It should match with the current hostname: <em>%s</em>'] = '将你的网站域名填入%s字段。域名应匹配当前主机名：<em>%s</em>';
+$lang['Set the <b>%s</b> to <em>%s</em>'] = '把<b>%s</b>设为<em>%s</em>';
+?>
