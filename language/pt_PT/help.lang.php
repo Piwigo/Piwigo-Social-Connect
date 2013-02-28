e07949c11c112f0fe5b30d37e2961780270fe7d6
mistic100 <mistic@piwigo.org>
2013-02-28 14:57:36 +0000

    restore some english names
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@21071 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pt_PT/help.lang.php
+++ language/pt_PT/help.lang.php
@@ -23,9 +23,9 @@
 $lang['Fill out any required fields such as the application name and description'] = 'Preencha os campos obrigatórios como o nome da aplicação e descrição';
 $lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Ir para <a href="%s" target="_blank">%s</a> e criar uma nova aplicação';
 $lang['No registration required for OpenID based providers'] = 'Registo não necessário para provedores baseados em OpenID ';
-$lang['On the <b>API Access</b> tab, <b>create an OAuth 2.0 Client ID</b>'] = 'No separador <b>Acesso API</b>, <b>crie um Client ID OAuth 2.0</b>';
-$lang['On the <b>Create Client ID</b> popup, switch to advanced settings by clicking on <b>(more options)</b>'] = 'No popup <b>Criar ID Cliente</b>, mude para definições avançadas clicando no <b>(mais opções)</b>';
-$lang['Once the application is registered update the permissions : set <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'Assim que aplicação esteja registada actualize as permissões: defina <b>Contactos</b> como <em>Leitura</em> e <b>Directoria Social</b> como <em>Leitura Pública</em>';
+$lang['On the <b>API Access</b> tab, <b>create an OAuth 2.0 Client ID</b>'] = 'No separador <b>API Access</b>, <b>crie um Client ID OAuth 2.0</b>';
+$lang['On the <b>Create Client ID</b> popup, switch to advanced settings by clicking on <b>(more options)</b>'] = 'No popup <b>Create Client ID</b>, mude para definições avançadas clicando no <b>(more options)</b>';
+$lang['Once the application is registered update the permissions : set <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'Assim que aplicação esteja registada actualize as permissões: defina <b>Contacts</b> como <em>Read</em> e <b>Social Directory</b> como <em>Read Public</em>';
 $lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Uma vez registado, copie e cole as credenciais criadas pela aplicação nesta página de instalação.';
 $lang['Provide this URL as the Callback/Redirect URL for your application: <em>%s</em>'] = 'Forneça este URL como o URL Callback/Redirect para a sua aplicação: <em>%s</em>';
 $lang['Put your website domain in the %s fields. It should match with the current hostname: <em>%s</em>'] = 'Coloque o domínio do seu website no campos %s. Deverá ser igual ao seu anfitrião actual:<em>%s</em>';
