519f9779d21f482f5253a2dedfa9af27602669d3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-02 23:44:04 +0000

    [oauth] Add hu_HU, thanks to : samli
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@21105 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Contacting <b>%s</b>. Please wait.'] = 'Kapcsolódás <b>%s</b>. Kérjük várjon.';
+$lang['OpenID must be enabled in order to use %s authentication'] = '%S-hitelesítés használatához engedélyezni kell az OpenID-t';
+$lang['You registered with a %s account, please sign in with the same account.'] = 'Regisztrált %s fiókkal, kérjük jelentkezzen be ugyanazzal a fiókkal.';
+$lang['Users are not allowed to register on your gallery. OAuth will not work correctly.'] = 'A regisztráció le van tiltva a galériában. Az OAuth nem fog megfelelően működni.';
+$lang['Your registration is almost done, please complete the registration form.'] = 'A regisztráció majdnem kész. Kérjük, töltse ki a teljes regisztrációs űrlapot.';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Hiba történt, kérjük lépjen kapcsolatba a galéria tulajdonosával. <i>Hibakód : %s</i>';
+$lang['Authentication canceled'] = 'Hitelesítési megszakítva';
+$lang['Display sign in buttons in the menubar'] = 'Bejelentkező gombok megjelenítése a menüsorban';
+$lang['Display sign in buttons on the register page'] = 'Bejelentkező gombok megjelenítése a regisztrációs oldalon';
+$lang['Icon size on the identification page'] = 'Ikon mérete az azonosítás oldalon';
+$lang['Icon size in the menubar'] = 'Ikon mérete a menüsorban';
+$lang['OAuth: PHP Curl extension is needed'] = 'OAuth: PHP Curl kiterjesztés szükséges';
+$lang['OAuth: You need to configure the credentials'] = 'OAuth: Be kell állítani a hitelesítő adatokat';
+$lang['Or sign in with'] = 'Vagy jelentkezzen be';
+$lang['Please enter your OpenID URL'] = 'Kérjük, adja meg az OpenID URL-jét';
+$lang['Please enter your username'] = 'Kérjük, adja meg a felhasználónevét';
+$lang['Providers'] = 'Szolgáltatók';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Sajnáljuk, a regisztráció tiltva van ebben a galériában.';
+$lang['%s: invalid keys'] = '%s: érvénytelen kulcs';
+$lang['Disabled'] = 'Kikapcsolva';
+$lang['Enabled'] = 'Bekapcsolva';
+$lang['Error...'] = 'Hiba...';
+$lang['Loading...'] = 'Betöltés...';
+$lang['Profile URL'] = 'Profil URL';
+?>
