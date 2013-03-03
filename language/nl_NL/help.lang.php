d4b49787364ef3046d1f8a084e966585c534eb2e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-03 18:28:22 +0000

    [oauth] Add nl_NL, thanks to : pauldaytona
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@21134 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/help.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['On the <b>Create Client ID</b> popup, switch to advanced settings by clicking on <b>(more options)</b>'] = 'Op de <b>Create Client ID</b> popup, schakel naar geavanceerde instellingen door te klikken op <b>(more options)</b>';
+$lang['Once the application is registered update the permissions : set <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'Als de applicatie is geregistreerd, vernieuw de rechten : set <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>';
+$lang['Provide this URL as the Callback/Redirect URL for your application: <em>%s</em>'] = 'Geef deze URL als de callback/Redirect URL voor je applicatie: <em>%s</em>';
+$lang['Put your website domain in the %s fields. It should match with the current hostname: <em>%s</em>'] = 'Vul je website domain in the %s velden. het moet overeenkomne met je huidige hostnaam <em>%s</em>';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Na registratie, kopieer en plak de aangemaakte applicatie aanmeldgegevens in deze setup pagina';
+$lang['Fill out any required fields such as the application name and description'] = 'Vul alle verplichte velden in zoals applicatie naam en omschrijving';
+$lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Ga naar <a href="%s" target="_blank">%s</a> en maak een nieuwe applicatie';
+$lang['No registration required for OpenID based providers'] = 'Geen registratie verplicht voor OpenID gebaseerde bronnen';
+$lang['On the <b>API Access</b> tab, <b>create an OAuth 2.0 Client ID</b>'] = 'Op de <b>API Access</b> tab, <b>maak een OAuth 2.0 Client ID</b>';
+$lang['Set the <b>%s</b> to <em>%s</em>'] = 'Zet de <b>%s</b> naar <em>%s</em>';
+?>
---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Er is een fout opgetreden, meldt het de gallerie eigenaar. <i>Error code : %s</i>';
+$lang['%s: invalid keys'] = '%s: ongeldige toetsen';
+$lang['Contacting <b>%s</b>. Please wait.'] = '<b>%s</b> wordt geconnect. Even geduld AUB';
+$lang['Display sign in buttons on the register page'] = 'Toon inlog knoppen op de registratie pagina';
+$lang['OAuth: PHP Curl extension is needed'] = 'OAuth: PHP Curl extensie is nodig';
+$lang['OAuth: You need to configure the credentials'] = 'OAuth: Je moet de aanmeldgegevens configureren';
+$lang['OpenID must be enabled in order to use %s authentication'] = 'OpenID moet geactiveerd zijn om deze vorm van aanmelden te gebruiken';
+$lang['Please enter your OpenID URL'] = 'Vul je OpenID url in';
+$lang['Authentication canceled'] = 'Aanmelden geanuleerd';
+$lang['Display sign in buttons in the menubar'] = 'Toon login knoppen in de menu balk';
+$lang['Icon size on the identification page'] = 'Icoon formaat op de identificatie pagina';
+$lang['Logged with'] = 'Ingelogd met';
+$lang['Or sign in with'] = 'Of log in met';
+$lang['Please enter your username'] = 'Vul je gebruikersnaam in';
+$lang['Providers'] = 'Dienstverleners';
+$lang['You registered with a %s account, please sign in with the same account.'] = 'Je bent geregistreerd met een %s account, log in met dezelfde account';
+$lang['Profile URL'] = 'Profiel URL';
+$lang['Sign in with %s'] = 'Log in met %s';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Sorry, nieuwe aanmeldingen zijn geblokkeerd in deze gallerie';
+$lang['Users are not allowed to register on your gallery. OAuth will not work correctly.'] = 'Gebruikers mogen niet registreren in je gallerie, OAuth werkt niet goed.';
+$lang['Your registration is almost done, please complete the registration form.'] = 'Je registratie is bijna klaar, maak het registratieformulier compleet.';
+$lang['Disabled'] = 'Uitgeschakeld';
+$lang['Enabled'] = 'Ingeschakeld';
+$lang['Error...'] = 'Fout...';
+$lang['Icon size in the menubar'] = 'Icoon formaat in de menubalk';
+$lang['Loading...'] = 'Laden..';
+?>
