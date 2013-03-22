ccde5e19a254363dadf3c6134a62870d12eb62ec
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-22 05:33:22 +0000

    [oauth] Update zh_TW, thanks to : tedx
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@21775 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Providers'] = '提供者';
+$lang['Sign in with %s'] = '登入為 %s';
+$lang['Sorry, new registrations are blocked on this gallery.'] = '很抱歉，新註冊的被封鎖在這個畫廊。';
+$lang['Users are not allowed to register on your gallery. OAuth will not work correctly.'] = '不允許用戶註冊在您的畫廊。 OAuth的將無法正常運作。';
+$lang['You registered with a %s account, please sign in with the same account.'] = '您註冊的 %s 帳戶，請登入相同的帳戶。';
+$lang['Your registration is almost done, please complete the registration form.'] = '您的註冊差不多完成，請完成註冊表格。';
+?>
