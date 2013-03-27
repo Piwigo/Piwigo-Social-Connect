7e708aa01664b881cf705d2c0c0d9535371ef435
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-27 05:55:57 +0000

    [oauth] Add th_TH, thanks to : pradit
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@21863 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/th_TH/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Providers'] = 'ผู้ให้บริการ';
+$lang['Sign in with %s'] = 'เข้าสู่ระบบโดย %s';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'ขออภัย สมาชิกใหม่จะถูกบล็อกในแกลเลอรี่นี้';
+$lang['Users are not allowed to register on your gallery. OAuth will not work correctly.'] = 'ผู้ใช้งานทั่วไปจะได้รับอนุญาตให้ลงทะเบียนในแกลเลอรี่ของคุณ, ปลั๊กอิน OAuth จะไม่ทำงานอย่างถูกต้อง';
+$lang['You registered with a %s account, please sign in with the same account.'] = 'คุณได้ลงทะเบียนกับบัญชี %s, โปรดลงชื่อเข้าใช้ด้วยบัญชีเดียวกัน';
+$lang['Your registration is almost done, please complete the registration form.'] = 'การลงทะเบียนของคุณเกือบจะสำเร็จแล้ว, โปรดกรอกแบบฟอร์มการลงทะเบียนที่เหลือให้ครบถ้วน';
+$lang['%s: invalid keys'] = '%s: คีย์ที่ไม่ถูกต้อง';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'เกิดข้อผิดพลาดโปรดติดต่อเจ้าของแกลเลอรี่ รหัสข้อผิดพลาด <i>: %s </i>';
+$lang['Authentication canceled'] = 'ยกเลิก การตรวจสอบ';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'กำลังติดต่อ <b>%s</b> โปรดรอสักครู่';
+$lang['Disabled'] = 'ปิดการใช้งาน';
+$lang['Display sign in buttons in the menubar'] = 'แสดงปุ่มเข้าสู่ระบบบนแถบเมนู';
+$lang['Display sign in buttons on the register page'] = 'แสดงปุ่มเข้าสู่ระบบในหน้าลงทะเบียน';
+$lang['Enabled'] = 'เปิดการใช้งาน';
+$lang['Error...'] = 'ข้อผิดพลาด ...';
+$lang['Icon size in the menubar'] = 'ขนาดไอคอนในแถบเมนู';
+$lang['Icon size on the identification page'] = 'ขนาดไอคอนบนหน้าโปรไฟล์';
+$lang['Loading...'] = 'กำลังโหลด ...';
+$lang['Logged with'] = 'เข้าสู่ระบบด้วย';
+$lang['OAuth: PHP Curl extension is needed'] = 'ปลัีกอิน OAuth: ต้องการส่วนขยาย PHP Curl';
+$lang['OAuth: You need to configure the credentials'] = 'ปลัีกอิน OAuth: คุณต้องกำหนดค่าข้อมูลก่อน';
+$lang['OpenID must be enabled in order to use %s authentication'] = 'OpenID ต้องเปิดใช้งานเพื่อที่จะใช้การตรวจสอบ %s';
+$lang['Or sign in with'] = 'หรือลงชื่อเข้าใช้ด้วย';
+$lang['Please enter your OpenID URL'] = 'โปรดใส่ URL OpenID ของคุณ';
+$lang['Please enter your username'] = 'กรุณาป้อนชื่อผู้ใช้';
+$lang['Profile URL'] = 'URL โปรไฟล์';
+?>
