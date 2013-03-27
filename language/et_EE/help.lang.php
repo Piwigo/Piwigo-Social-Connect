e07277c21327bf1071cf9f5218adcc14a287ce5a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-27 15:29:54 +0000

    [oauth] Add et_EE, thanks to : Eggy
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@21872 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/et_EE/help.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Set the <b>%s</b> to <em>%s</em>'] = 'Seadista <b>%s</b> järgmiseks <em>%s</em>';
+$lang['Provide this URL as the Callback/Redirect URL for your application: <em>%s</em>'] = 'Paku seda URL\'i ümbersuunamiseks sinu aplikatsioonile: <em>%s</em>';
+$lang['Put your website domain in the %s fields. It should match with the current hostname: <em>%s</em>'] = 'Pane oma veebilehe domeen %s väljale. See peab olema sama mis praegune hosti nimi: <em>%s</em>';
+$lang['Once the application is registered update the permissions : set <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'Kui aplikatsioon on registreeritud uuenda õigusi : sea <b>Contacts</b> kui <em>Read</em> ja <b>Social Directory</b> kui <em>Read Public</em>';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Pärast registreerimist kopeeri loodud aplikatsiooni info seadistuse lehele';
+$lang['On the <b>Create Client ID</b> popup, switch to advanced settings by clicking on <b>(more options)</b>'] = '<b>Create Client ID</b> hüpikaknas lülita edasijõudnute seadistuse lehele klõpsates <b>(more options)</b>';
+$lang['Fill out any required fields such as the application name and description'] = 'Täida kõik vajalikud väljad nagu nimi ja kirjeldus';
+$lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Mine <a href="%s" target="_blank">%s</a> ja loo uus aplikatsioon';
+$lang['No registration required for OpenID based providers'] = 'Registreerimine ei ole vajalik OpenID\'d toetavatele teenusepakkujatele';
+$lang['On the <b>API Access</b> tab, <b>create an OAuth 2.0 Client ID</b>'] = '<b>API Access</b> lehel, <b>loo OAuth 2.0 Client ID</b>';
+?>
---
+++ language/et_EE/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['OpenID must be enabled in order to use %s authentication'] = 'OpenID peab olema aktiveeritud, et kasutada %s audentimist';
+$lang['Or sign in with'] = 'Või logi sisse kasutades';
+$lang['Please enter your OpenID URL'] = 'Palun sisesta oma OpenID URL';
+$lang['Please enter your username'] = 'Palun sisesta oma kasutajanimi';
+$lang['Profile URL'] = 'Profiili URL';
+$lang['Providers'] = 'Pakkuja';
+$lang['Sign in with %s'] = 'Logi sisse kasutjaga %';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Vabandust, uusi kasutajaid ei saa siin galeriis lisada';
+$lang['Users are not allowed to register on your gallery. OAuth will not work correctly.'] = 'Kasutjatel pole lubatud sinu galeriis registreerida. OAuth ei tööta korralikult. ';
+$lang['You registered with a %s account, please sign in with the same account.'] = 'Sa registreerisid %s kasutjaga, palun logi sisse sama infoga';
+$lang['Your registration is almost done, please complete the registration form.'] = 'Registreerimine on peaagu lõpetatud, palun lõpeta registreerimine.';
+$lang['%s: invalid keys'] = '%s: mittesobivad sissekanded';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Esines viga, kontakteeru galerii omanikuga. <i>Veakood: %s</i>';
+$lang['Authentication canceled'] = 'Audentimine katkestatud';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'Ühendame <b>%s</b>. Palun oota...';
+$lang['Disabled'] = 'Deaktiveeritud';
+$lang['Display sign in buttons in the menubar'] = 'Näita sisselogimise nuppe menüüribal';
+$lang['Display sign in buttons on the register page'] = 'Näita sisselogimise nuppe registreerimise lehel';
+$lang['Enabled'] = 'Aktiveeritud';
+$lang['Error...'] = 'Viga...';
+$lang['Icon size in the menubar'] = 'Ikooni suurus menüü ribal';
+$lang['Icon size on the identification page'] = 'Ikooni suurus identifitseerimise lehel';
+$lang['Loading...'] = 'Laadimine...';
+$lang['Logged with'] = 'Sisse logitud';
+$lang['OAuth: PHP Curl extension is needed'] = 'OAuth: PHP Curl laiendus on vajalik';
+$lang['OAuth: You need to configure the credentials'] = 'OAuth: ';
+?>
