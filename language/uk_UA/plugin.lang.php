5a83017835629bc18c7c70e5b11c36e788196307
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-01 18:50:38 +0000

    [oauth] Add uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@24717 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['You registered with a %s account, please sign in with the same account.'] = 'Ви зареєстровані з облікового запису %s, будь ласка, увійдіть на сайт тим самим обліковим записом.';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Сталася помилка, зверніться до власника галереї. <i>Код помилки: %s</i>.';
+$lang['Authentication canceled'] = 'Автентифікацію скасовано';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'З’єднання <b>%s</b>. Будь ласка, зачекайте.';
+$lang['Display sign in buttons in the menubar'] = 'Показати кнопки для входу в меню';
+$lang['Display sign in buttons on the register page'] = 'Показати кнопки для входу на сторінці Реєстрація';
+$lang['Icon size in the menubar'] = 'Розмір піктограм у меню';
+$lang['Icon size on the identification page'] = 'Розмір піктограм на сторінці ідентифікації';
+$lang['OAuth: PHP Curl extension is needed'] = 'OAuth: Необхідно розширення PHP Curl';
+$lang['OAuth: You need to configure the credentials'] = 'OAuth: Потрібно налаштувати облікові дані';
+$lang['OpenID must be enabled in order to use %s authentication'] = 'OpenID повинен бути включений для того, щоб використовувати аутентифікацію %s';
+$lang['Please enter your OpenID URL'] = 'Будь ласка, введіть ваш URL OpenID';
+$lang['Please enter your username'] = 'Введіть своє ім’я користувача';
+$lang['%s: invalid keys'] = '%s: непрацездатні ключі';
+$lang['Logged with'] = 'Зареєстрований як';
+$lang['Profile URL'] = 'URL профілю';
+$lang['Disabled'] = 'Вимкнено';
+$lang['Enabled'] = 'Ввімкнено';
+$lang['Error...'] = 'Помилка...';
+$lang['Loading...'] = 'Завантаження...';
+$lang['Or sign in with'] = 'Або увійдіть через';
+$lang['Providers'] = 'Провайдери';
+$lang['Sign in with %s'] = 'Увійдіть через %s';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'На жаль, нові реєстрації блокуються на цій галереї.';
+$lang['Users are not allowed to register on your gallery. OAuth will not work correctly.'] = 'Користувачі не можуть зареєструватися у вашій галереї. OAuth не працюватиме коректно.';
+$lang['Your registration is almost done, please complete the registration form.'] = 'Ваша реєстрація майже закінчена, заповніть реєстраційну форму.';
+?>
