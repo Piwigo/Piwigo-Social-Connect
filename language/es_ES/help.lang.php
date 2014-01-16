6098bd8d6c2303bf12db2978e3202686a29d38a2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-16 09:38:11 +0000

    [oauth] Update es_ES, thanks to : jpr928
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@26800 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/es_ES/help.lang.php
+++ language/es_ES/help.lang.php
@@ -20,14 +20,15 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['On the <b>Create Client ID</b> popup, switch to advanced settings by clicking on <b>(more options)</b>'] = 'En la popup <b>Create Client ID</b>, hagan clic en <b>(more options)</b>';
 $lang['Once the application is registered update the permissions : set <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'Cuando la aplicación está registrada, editar los permisos: seleccione <em>Read</em> para <b>Contacts</b> y <em>Read Public</em> para <b>Social Directory</b>';
 $lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Cuando su solicitud sea registrada, copie y pegue las claves de autentificación en esta página';
 $lang['Provide this URL as the Callback/Redirect URL for your application: <em>%s</em>'] = 'Dar esta URL como  Callback/Redirect URL para su aplicación';
 $lang['Put your website domain in the %s fields. It should match with the current hostname: <em>%s</em>'] = 'Ponga su nombre de dominio en los campos %s. Debe corresponder a <em>%s</em>';
 $lang['Set <b>%s</b> to <em>%s</em>'] = 'Seleccionar <b>%s</b> para <em>%s</em>';
-$lang['On the <b>API Access</b> tab, <b>create an OAuth 2.0 Client ID</b>'] = 'En la pestaña <b>API Access</b>, hagan clic en <b>create an OAuth 2.0 Client ID</b>';
-$lang['No registration required for OpenID based providers'] = 'No es necesario registrarse para tipos de autenticación OpenID';
 $lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Vallan a <a href="%s" target="_blank">%s</a> y crean una nueva aplicación';
 $lang['Fill out any required fields such as the application name and description'] = 'Rellene todos los campos necesarios como el nombre y la descripción de la aplicación';
+$lang['Based on OpenID'] = 'Basado en OpenID';
+$lang['Go to <b>Settings->Advanced</b> and activate <em>Client OAuth Login</em>'] = 'Ir a <b>Opciones->Avanzada</b> y activar <em> OAuth Acceso Clientes</em>';
+$lang['No registration required'] = 'Identificación no es necesaria';
+$lang['On the <b>APIs & auth -> Credentials</b> tab, <b>Create new client ID</b>'] = 'En la <b>APIs & auth -> Credenciales</b>, <b>Crear nuevo ID de cliente</b>';
 ?>
