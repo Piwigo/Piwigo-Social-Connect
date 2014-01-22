371bb096bcc2a47f134979eb44428afe013c4056
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-22 21:51:41 +0000

    [oauth] Update eo_EO, thanks to : sylvbarrier
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@26910 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/eo_EO/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s: invalid keys'] = '%s: nevalidaj ŝlosiloj';
+$lang['Cancel'] = 'Forigi';
+$lang['Enabled'] = 'Permesita';
+$lang['Error...'] = 'Eraro...';
+$lang['Or sign in with'] = 'Aŭ ensalutu kun';
+$lang['Please enter your user ID'] = 'Bonvolu entajpi vian uzantan identigilon';
+$lang['Please enter your username'] = 'Bonvolu entajpi vian salutnomo';
+?>
