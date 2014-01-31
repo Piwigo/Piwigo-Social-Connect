23c5066f3ae78dc4642350613e16f839c9650e5a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-31 04:46:36 +0000

    [oauth] Update eo_EO, thanks to : sylvbarrier
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@27087 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/eo_EO/help.lang.php
+++ language/eo_EO/help.lang.php
@@ -24,4 +24,7 @@ $lang['Based on OpenID'] = 'Bazita sur OpenID';
 $lang['No registration required'] = 'Necesas neniu registrado';
 $lang['Provide this URL as the Callback/Redirect URL for your application: <em>%s</em>'] = 'Provizi tiun retadreson kiel la Revoka/Alidirekta retadreso por via aplikaĵo: <em>%s</em>';
 $lang['Set <b>%s</b> to <em>%s</em>'] = 'Ŝalti <b>%s</b> al <em>%s</em>';
+$lang['Fill out any required fields such as the application name and description'] = 'Plenigu ĉiuj necesaj tajpujoj kiel la nomo kaj la priskribo de la aplikaĵo';
+$lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Iri al <a href="%s" target="_blank">%s</a> kaj krei novan aplikaĵon';
+$lang['Go to <b>Settings->Advanced</b> and activate <em>Client OAuth Login</em>'] = 'Iri al <b>Agordoj->Detalaj</b> kaj aktivigi <em>Clien OAuth Login</em>';
 ?>
