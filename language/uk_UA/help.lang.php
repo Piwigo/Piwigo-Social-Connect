f7afc492e6ae926028565fcb62b7260196f2eb28
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-02-08 08:03:55 +0000

    [oauth] Update uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@27262 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/uk_UA/help.lang.php
+++ language/uk_UA/help.lang.php
@@ -22,4 +22,13 @@
 // +-----------------------------------------------------------------------+
 $lang['Fill out any required fields such as the application name and description'] = 'Заповніть всі необхідні поля, такі як ім’я та опис програми';
 $lang['Set <b>%s</b> to <em>%s</em>'] = 'Встановіть <b>%s</b> з <em>%s</em>';
+$lang['Once the application is registered update the permissions : set <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'Після того, як додаток зареєструється, оновіть дозволи: встановіть <b>Контакти</b> як <em>Read</em> і <b>Соціального каталог</b> як <em>Read Public</em>';
+$lang['Go to <b>Settings->Advanced</b> and activate <em>Client OAuth Login</em>'] = 'Перейти до <b>Налаштування-> Додатково</b> та активувати <em>Користувацький OAuth Логін<\/em>';
+$lang['On the <b>APIs & auth -> Credentials</b> tab, <b>Create new client ID</b>'] = 'На вкладці <b>APIs & auth -> Облікові дані</b>, <b>Створити новий ID клієнта</b>';
+$lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Перейти до <a href="%s" target="_blank">%s</a> і створити новий додаток';
+$lang['Put your website domain in the %s fields. It should match with the current hostname: <em>%s</em>'] = 'Введіть домен сайту в полі %s. Він повинен відповідати поточному хоста: <em>%s</em>';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Після реєстрації скопіюйте і вставте реєстраційні дані додатку на сторінці налаштувань';
+$lang['Based on OpenID'] = 'На підставі OpenID';
+$lang['No registration required'] = 'Не потрібна реєстрація';
+$lang['Provide this URL as the Callback/Redirect URL for your application: <em>%s</em>'] = 'Забезпечити цю адресу як Callback/Redirect URL для вашого додатку: <em>%s</em>';
 ?>
