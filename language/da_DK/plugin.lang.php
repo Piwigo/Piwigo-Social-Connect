32cd310c92c69977abd2b838dd7776e5c09c6d30
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-06-19 05:17:42 +0000

    [oauth] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@28727 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/da_DK/plugin.lang.php
+++ language/da_DK/plugin.lang.php
@@ -49,4 +49,4 @@ $lang['Allow users to merge existing account with new <i>Social Connect</i> iden
 $lang['Associate with an existing account'] = 'Knyt til en eksisterende konto';
 $lang['Cancel'] = 'Annuller';
 $lang['Please enter your user ID'] = 'Angiv din bruger-id';
-?>
+$lang['For security reason, the main webmaster account can\'t be merged with a remote account, but you can use another webmaster account.'] = 'Af sikkerhedsårsager kan den primære webmasterkonto ikke blive lagt sammen med en fjernkonto, men man kan benytte en anden webmasterkonto.';
