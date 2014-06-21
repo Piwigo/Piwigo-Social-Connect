e82d52829df530003a452a6734ffa670cb75fa42
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-06-21 12:44:03 +0000

    [oauth] Update it_IT, thanks to : Ericnet
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@28758 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -49,4 +49,4 @@ $lang['Allow users to merge existing account with new <i>Social Connect</i> iden
 $lang['Associate with an existing account'] = 'Associa con un account esistente';
 $lang['Please enter your user ID'] = 'Prego inserisci il tuo ID utente';
 $lang['Cancel'] = 'Cancella';
-?>
+$lang['For security reason, the main webmaster account can\'t be merged with a remote account, but you can use another webmaster account.'] = 'Per motivi di sicurezza, l\'account webmaster principale non può essere unito con un account remoto, ma puoi utilizzare un altro account webmaster.';
