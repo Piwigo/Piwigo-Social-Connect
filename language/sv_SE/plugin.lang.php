2e9207fde609ba0a6e74244cde7b9af635fa6c18
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-06-25 10:08:14 +0000

    [oauth] Update sv_SE, thanks to : Blytung
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@28790 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,52 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Profile URL'] = 'Profil URL';
+$lang['Providers'] = 'Leverantörer';
+$lang['Sign in with %s'] = 'Logga in med %s';
+$lang['Social Connect: PHP Curl extension is needed'] = 'Social Connect: PHP Curl-tillägget krävs';
+$lang['Social Connect: You need to configure the credentials'] = 'Social Connect: Du måste konfigurera inloggningsalternativen';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Tyvärr så är nya registreringar stoppade på denna sida.';
+$lang['Users are not allowed to register on your gallery. Social Connect will not work correctly.'] = 'Användare tillåts inte att registrera på ditt galleri. Social Connect kommer inte att fungera.';
+$lang['You registered with a %s account, please sign in with the same account.'] = 'Du registrerades med ett %s konto, vänligen logga in med det samma.';
+$lang['Your registration is almost done, please complete the registration form.'] = 'Din registrering är nästan klar, vänligen slutför registreringen med hjälp av formuläret.';
+$lang['Please enter your username'] = 'Vänligen skriv in ditt användarnamn';
+$lang['%s: invalid keys'] = '%s: ogiltiga nycklar';
+$lang['Allow users to merge existing account with new <i>Social Connect</i> identity'] = 'Tillåt användare att sammanfoga befintligt konto med den nya <i>Social Connect</i> identiteten';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Ett fel inträffade, vänligen kontakta sidans ägare. Meddela felmeddelandet: <i>Error code: %s</i>';
+$lang['Associate with an existing account'] = 'Associera med ett befintligt konto';
+$lang['Authentication canceled'] = 'autentisering avbruten';
+$lang['Cancel'] = 'Avbryt';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'Kontaktar <b>%s</b>. Vänligen vänta.';
+$lang['Disabled'] = 'Inaktiverad';
+$lang['Display sign in buttons in the menubar'] = 'Visa inloggningsikoner i menylisten';
+$lang['Display sign in buttons on the register page'] = 'Visa inloggningsikoner på registreringssidan';
+$lang['Enabled'] = 'Aktiverad';
+$lang['Error...'] = 'Error...';
+$lang['For security reason, the main webmaster account can\'t be merged with a remote account, but you can use another webmaster account.'] = 'Av säkerhetsskäl kan huvud-webmaster-kontot inte slås samman med ett fjärrkonto, men du kan använda ett annat webmaster-konto.';
+$lang['Icon size in the menubar'] = 'Ikonens storlek i menylisten';
+$lang['Icon size on the identification page'] = 'Ikonens storlek på inloggningssidan';
+$lang['Loading...'] = 'Laddar...';
+$lang['Logged with'] = 'Loggad med';
+$lang['Or sign in with'] = 'Eller logga in med';
+$lang['Please enter your OpenID URL'] = 'Vänligen skriv in din OpenID URL';
+$lang['Please enter your user ID'] = 'Vänligen skriv in ditt användarnamn';
