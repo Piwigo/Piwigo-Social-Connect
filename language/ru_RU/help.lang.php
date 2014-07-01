a5510416365ee8671eb83a31b05882413d385665
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-07-01 17:28:41 +0000

    [oauth] Update ru_RU, thanks to : Konve
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@28907 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ru_RU/help.lang.php
+++ language/ru_RU/help.lang.php
@@ -23,8 +23,14 @@
 $lang['Fill out any required fields such as the application name and description'] = 'Заполните все необходимые поля, включая название приложения и описание ';
 $lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Пройдите по ссылке <a href="%s" target="_blank">%s</a> и создайте новое приложение';
 $lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'После регистрации скопируйте и вставьте регистрационные данные приложения на странице настроек';
-$lang['Based on OpenID'] = 'Основано на OpenID';
+$lang['Based on OpenID'] = 'Базируется на OpenID';
 $lang['No registration required'] = 'Регистрация не требуется';
 
 ';
-$lang['Set <b>%1s</b> to <em>%2s</em>'] = 'Установить <b>%1s</b> для <em>%2s</em>';
+$lang['Set <b>%1s</b> to <em>%2s</em>'] = 'Установить <b>%1s</b> в <em>%2s</em>';
+$lang['Once the process is finished, click on the newly created project'] = 'После завершения процесса нажмите на вновь созданный проект';
+$lang['Once the application is created, click <b>See application details</b>'] = 'После создания приложения, нажмите кнопку <b>Смотреть детали </b>';
+$lang['Go to <b>Settings -> Advanced</b> and set <b>Valid OAuth redirect URIs</b> to <em>%s</em>'] = 'Перейти в <b>Установки -> Расширенные</b> и установить <b>проверку идентификации перенаправления URIs</b> для <em>%s</em>';
+$lang['Go to <b>API Parameters</b> and set <em>%s</em> for <b>Redirect URL</b>'] = 'Перейти в <b>Параметры API</b> и установить <em>%s</em> для <b>Перенаправления URL</b>';
+$lang['Go to <b>Settings -> Basic</b> and fill the contact email'] = 'Перейти в <b>Установки -> Основные</b> и ввести контактный емайл';
+$lang['Enter <em>%1s</em> for <b>%2s</b>'] = 'Ввод <em>%1s</em> для <b>%2s</b>';
