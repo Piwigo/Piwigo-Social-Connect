d236779019c7d560b16d08f9bd68515fc5c5dfd2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-07-06 15:33:18 +0000

    [oauth] Update pt_BR, thanks to : flaviove
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@28971 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pt_BR/help.lang.php
+++ language/pt_BR/help.lang.php
@@ -20,15 +20,20 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['Once the application is registered update the permissions : set <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'Uma vez que o aplicativo está registrado, atualizar as permissões: configurar <b>Contatos</b> como <em>Leia</em> e <b>Diretório Social </b> como <em>Leitura Pública</em>';
 $lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Depois de ter se registado, copiar e passar as credenciais de aplicativos criados para esta página de configuração';
-$lang['Provide this URL as the Callback/Redirect URL for your application: <em>%s</em>'] = 'Fornecer essa URL como URL de retorno/redirecionamento para sua aplicação: <em>%s</em>';
-$lang['Put your website domain in the %s fields. It should match with the current hostname: <em>%s</em>'] = 'Coloque o seu domínio do site nos campos %s. Ele deve combinar com o nome da máquina atual: <em>%s</em>';
-$lang['Set <b>%s</b> to <em>%s</em>'] = 'Defina o <b>%s</b> para <em>%s</ em>';
 $lang['Fill out any required fields such as the application name and description'] = 'Preencha todos os campos necessários, tais como o nome do aplicativo e descrição';
 $lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Ir para <a href="%s"target="_blank">%s</a> e criar uma nova aplicação';
 $lang['Based on OpenID'] = 'Baseado em OpenID';
-$lang['Go to <b>Settings->Advanced</b> and activate <em>Client OAuth Login</em>'] = 'Ir para <b>Settings->Aavançado</b> e ative <em>Cliente Auto Login</em>';
 $lang['No registration required'] = 'Sem necessidade de registro';
-$lang['On the <b>APIs & auth -> Credentials</b> tab, <b>Create new client ID</b>'] = 'Na aba <b>APIs & Auto-> Credenciais</b>, <b> Criar nova ID de cliente</b>';
-?>
+$lang['Set <b>%1s</b> to <em>%2s</em>'] = 'Configurar <b>%1s</b> para <em>%2s</em>';
+$lang['Select these APIs: <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'Selecione essas APIs: <b>Contatos</b> como <em>Ler</em> e <b> Diretório Social </b> como <em> Leitura Pública </em>';
+$lang['Put your website domain in the <b>%1s</b> field. It must match with the current hostname: <em>%2s</em>'] = 'Coloque o seu domínio do site no campo <b>%1s</b>. Ele deve corresponder com o nome de host atual: <em>%2 </em>';
+$lang['Once the process is finished, click on the newly created project'] = 'Uma vez que o processo foi concluído, clique no projeto recém-criado';
+$lang['Once the application is created, click <b>See application details</b>'] = 'Uma vêz que a aplicação foi criada, click<b>Ver detelhes da aplicaçaõ</b>';
+$lang['On the <b>APIS & AUTH -> Credentials</b> tab, and click <b>Create new client ID</b>'] = 'Em <b>APIS & AUTH -> Credenciais</b>, click <b>Criar novo cliente ID</b>';
+$lang['Go to <b>Status & Review</b> and set the app public by clicking the big button on top-right'] = 'Ir para <b> Estado & Revisão </b> e definir o aplicativo público clicando no botão grande na parte superior direita';
+$lang['Go to <b>Settings -> Basic</b> and fill the contact email'] = 'Ir para <b>Configurações -> Básicas</b> e preencha um email para contato';
+$lang['Go to <b>Settings -> Advanced</b> and set <b>Valid OAuth redirect URIs</b> to <em>%s</em>'] = 'Ir para <b>Configurações -> Avançadas</b> e configure <b>Válido redirecionamento automático para URIs</b> para <em>%s</em>';
+$lang['Go to <b>API Parameters</b> and set <em>%s</em> for <b>Redirect URL</b>'] = 'Ir para <b>Parametros API</b> e configure <em>%s</em> para <b>Redirecionar URL</b>';
+$lang['Enter <em>%1s</em> for <b>%2s</b>'] = 'Entre <em>%1s</em> para <b>%2s</b>';
+$lang['Click on <b>Add Platform</b>, choose <b>Website</b> and set the <b>Site URL</b> to <em>%s</em>'] = 'Clique em <b>Adicionar plataforma</b>, escolha <b>o Website </b> e defina a <b>URL do Website </b> para <em>%s</em>';
