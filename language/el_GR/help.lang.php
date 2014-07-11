4a82708289af15e7a484ec4a2faa652831e350ee
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-07-11 18:19:37 +0000

    [oauth] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@29026 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/el_GR/help.lang.php
+++ language/el_GR/help.lang.php
@@ -20,15 +20,14 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['Put your website domain in the %s fields. It should match with the current hostname: <em>%s</em>'] = 'Βάλτε τομέα ιστοσελίδα σας στα %s πεδία. Θα πρέπει να ταιριάζει με το τρέχον hostname <em>%s</em>';
-$lang['Provide this URL as the Callback/Redirect URL for your application: <em>%s</em>'] = 'Δώστε αυτό το URL ως το Callback/Redirect URL για την εφαρμογή σας: <em>%s</em>';
 $lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Μόλις κάνετε την εγγραφή, κάντε αντιγραφή και επικόλληση τα διαπιστευτήρια της εφαρμογής που δημιουργήσατε σε αυτή τη σελίδα ρύθμισης';
 $lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Μετάβαση στο <a href="%s" target="_blank">%s</a> και δημιουργήστε μια νέα εφαρμογή';
 $lang['Fill out any required fields such as the application name and description'] = 'Συμπληρώστε όλα τα απαιτούμενα πεδία, όπως το όνομα της εφαρμογής και περιγραφή';
-$lang['Set <b>%s</b> to <em>%s</em>'] = 'Ρυθμίστε το <b>%s</b> σε <em>%s</em>';
-$lang['On the <b>APIs & auth -> Credentials</b> tab, <b>Create new client ID</b>'] = 'Στο <b>APIs & auth -> Credentials</b>, <b>Δημιουργία νέας ταυτότητας πελάτη</b>';
 $lang['No registration required'] = 'Δεν απαιτείται εγγραφή';
-$lang['Go to <b>Settings->Advanced</b> and activate <em>Client OAuth Login</em>'] = 'Πηγαίνετε στο <b>Ρυθμίσεις -> Για προχωρημένους</b> και ενεργοποιήστε πελάτη <em>Client OAuth Login</em>';
 $lang['Based on OpenID'] = 'Με βάση το OpenID';
-$lang['Select these APIs: <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> <em>Read Public</em>'] = 'Επιλέξτε αυτά τα API:<b> Επαφές </b> όπως <em> Διαβάστε </em> και <b> Κοινωνικός Κατάλογος</b> <em> Διαβάστε δημόσια </em>';
 $lang['Once the application is created, click <b>See application details</b>'] = 'Αφού δημιουργηθεί η εφαρμογή, κάντε κλικ στο <b>Δείτε λεπτομέρειες εφαρμογής</b>';
+$lang['Go to <b>Settings -> Advanced</b> and set <b>Valid OAuth redirect URIs</b> to <em>%s</em>'] = 'Πηγαίνετε στο  <b>Ρυθμίσεις -> Για προχωρημένους</b> και ρυθμίστε το <b>Ισχύων OAuth URIs ανακατεύθηνσης</b> σε <em>%s</em>';
+$lang['Go to <b>Settings -> Basic</b> and fill the contact email'] = 'Πηγαίνετε στο <b>Ρυθμίσεις -> Βασικά</b> και συμπληρώσετε το e-mail επικοινωνίας.';
+$lang['Go to <b>API Parameters</b> and set <em>%s</em> for <b>Redirect URL</b>'] = 'Πηγαίνετε στο <b>API Parameters</b> και ρυθμίστε <em>%s</em> για το <b>Redirect URL</b>';
+$lang['Enter <em>%1s</em> for <b>%2s</b>'] = 'Εισάγετε  <em>%1s</em> για το <b>%2s</b>';
+$lang['Click on <b>Add Platform</b>, choose <b>Website</b> and set the <b>Site URL</b> to <em>%s</em>'] = 'Κάντε κλικ στο <b>Προσθήκη Πλατφόρμας</b>, επιλέξτε <b>Website</b>  και ρυθμίστε τη <b>Site URL</b> για το <em>%s</em>';
