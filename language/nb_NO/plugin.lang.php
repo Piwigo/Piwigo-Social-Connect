9a23d19cf983a2c07fb10c0f37148875b8780918
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-11 19:10:52 +0000

    [oauth] Update nb_NO, thanks to : paulen
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@30006 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Providers'] = 'Tilbydere';
+$lang['Sign in with %s'] = 'Logg inn med %s';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Beklager, nye registreringer er blokkert for dette galleriet.';
+$lang['Social Connect: You need to configure the credentials'] = 'Social Connect: Du må konfigurere legitimasjon';
+$lang['Social Connect: PHP Curl extension is needed'] = 'Social Connect: PHP Curl filtype er nødvendig';
+$lang['Profile URL'] = 'Profile URL';
+$lang['Please enter your username'] = 'Skriv inn ditt brukernavn';
+$lang['Please enter your user ID'] = 'Skriv inn din bruker-ID';
+$lang['Or sign in with'] = 'Eller log inn med';
+$lang['Please enter your OpenID URL'] = 'Kriv inn din åpneID URL';
+$lang['Loading...'] = 'Laster...';
+$lang['Logged with'] = 'Logges med';
+$lang['Icon size on the identification page'] = 'Ikonstørrelsen på identifikasjonssiden';
+$lang['Icon size in the menubar'] = 'Ikonstørrelsen på menylinja';
+$lang['For security reason, the main webmaster account can\'t be merged with a remote account, but you can use another webmaster account.'] = 'Av sikkerhetsmessige årsaker kan hoved webmaster kontoen ikke bli slått sammen med en ekstern konto, men du kan bruke en annen webmaster konto.';
+$lang['Error...'] = 'Feil...';
+$lang['Enabled'] = 'Aktivert';
+$lang['Display sign in buttons on the register page'] = 'Vis logg inn knapp på registerings siden';
+$lang['Display sign in buttons in the menubar'] = 'Vis logg inn knapp i menylinja';
+$lang['Disabled'] = 'Deaktivere';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'Kontakter <b>%s</b>. Vennligst vent.';
+$lang['Close'] = 'Lukke';
+$lang['Cancel'] = 'Avbryt';
+$lang['Authentication canceled'] = 'Identitetsbekreftelse kansellert';
+$lang['Associate with an existing account'] = 'Forbinder med en eksisterende konto';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Det oppstod en feil, vennligst kontakt galleriets eier. <i>Feilkode:%s</i>';
+$lang['Allow users to merge existing account with new <i>Social Connect</i> identity'] = 'Tillat brukere å slå sammen eksisterende konto med ny <i>Social Connect </i> identitet';
+$lang['%s: invalid keys'] = '%s: ugyldig nøkkel';
