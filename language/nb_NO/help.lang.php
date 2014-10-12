413f22ef3ab4dbc9f8d47cad131d66adf9b53e9b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-12 05:35:43 +0000

    [oauth] Update nb_NO, thanks to : paulen
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@30011 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nb_NO/help.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['On the <b>APIS & AUTH -> Credentials</b> tab, and click <b>Create new client ID</b>'] = 'PÅ <b>APIS & AUTH -> legitimasjon</b> fanen, og klikk <b>Opprett ny klient ID</b>';
+$lang['No registration required'] = 'Ingen registreing nødvendig';
+$lang['Go to the <b>Settings</b> tab after creating the app'] = 'Gå til <b>Innstillinger</b> fanen etter å laget appen';
+$lang['Go to <b>Status & Review</b> and set the app public by clicking the big button on top-right'] = 'Gå til <b>Status & Gjennomgang</b> og sett apppen til offentlig ved å klikke på den store knappen oppe til høyre';
+$lang['Go to <b>Settings -> Basic</b> and fill the contact email'] = 'Gå til <b>Innstillinger -> Basic</b> og fyll ut kontakt e-post';
+$lang['Go to <b>Settings -> Advanced</b> and set <b>Valid OAuth redirect URIs</b> to <em>%s</em>'] = 'Gå til <b>Innstillinger -> Avanset</b> og sett <b>Gyldig Auto omdirigering URIs</b> til <em>%s</em>';
+$lang['Go to <b>API Parameters</b> and set <em>%s</em> for <b>Redirect URL</b>'] = 'Gå til <b>API Parameters</b> og set <em>%s</em> for å <b>Omdirigere URL</b>';
+$lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Gå til <a href="%s" target="_blank">%s</a> og opprette en ny søknad';
+$lang['Fill out any required fields such as the application name and description'] = 'Fyll ut alle obligatoriske felter som programnavnet og beskrivelse';
+$lang['Enter <em>%1s</em> for <b>%2s</b>'] = 'Enter <em>%1s</em> for <b>%2s</b>';
+$lang['Click on <b>Add Platform</b>, choose <b>Website</b> and set the <b>Site URL</b> to <em>%s</em>'] = 'Klikk på <b>Add Platform</b>, velg <b>Webside</b> og skriv inn <b>Sidens URL</b> til <em>%s</em>';
+$lang['Based on OpenID'] = 'Basert på åpenID';
