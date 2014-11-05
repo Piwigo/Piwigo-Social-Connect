c033dca498e0663678d6110e81cd82f558215bd8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-11-05 06:45:57 +0000

    [oauth] Update vi_VN, thanks to thanhphan467
    git-svn-id: http://piwigo.org/svn/extensions/oAuth@30366 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/vi_VN/plugin.lang.php
@@ -0,0 +1,55 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Please enter your user ID'] = 'Vui lòng điền Mã tài khoản của bạn';
+$lang['Social Connect: PHP Curl extension is needed'] = 'Mạng xã hội: Tiện ích mở rộng PHP Curl là cần thiết';
+$lang['For security reason, the main webmaster account can\'t be merged with a remote account, but you can use another webmaster account.'] = 'Vì lí do bảo mật, Tài khoản quản trị không thể nhập chung với một tài khoản từ xa, nhưng bạn có thể sử dụng tài khoản quản trị khác';
+$lang['Please enter your username'] = 'Vui lòng điền tên tài khoản của bạn';
+$lang['Profile URL'] = 'Trang cá nhân';
+$lang['Providers'] = 'Cung cấp';
+$lang['Sign in with %s'] = 'Đăng nhập với %s';
+$lang['Social Connect: You need to configure the credentials'] = 'Mạng xã hội: Bạn cần phải cấu hình các thông tin';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Xin lỗi, Đăng nhập mới của bạn bị khóa trong bộ sưu tập này';
+$lang['Unable to perform login'] = 'Không thể đăng nhập';
+$lang['User not found'] = 'Không tìm thấy tài khoản';
+$lang['Users are not allowed to register on your gallery. Social Connect will not work correctly.'] = 'Tài khoản không được chấp thuận để đăng ký trong bộ sưu tập của bạn. Mạng xã hội sẽ hoạt động không chính xác';
+$lang['You registered with a %s account, please sign in with the same account.'] = 'Bạn đã đăng ký với một %s tài khoản, vui lòng nhập tài khoản.';
+$lang['Your registration is almost done, please complete the registration form.'] = 'Bạn sắp hoàn thành thủ tục đăng ký, vui lòng điền vào mẫu đăng ký';
+$lang['Authentication canceled'] = 'Chứng thực bị hủy bỏ';
+$lang['Cancel'] = 'Hủy bỏ';
+$lang['Close'] = 'Đóng';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'Đang liên hệ <b>%s</b> Vui lòng chờ.';
+$lang['Disabled'] = 'Tắt';
+$lang['Display sign in buttons in the menubar'] = 'Hiển thị nút đăng nhập ở thanh công cụ';
+$lang['Display sign in buttons on the register page'] = 'Hiển thị nút đăng nhập trong trang đăng ký';
+$lang['Enabled'] = 'Bật';
+$lang['Error...'] = 'Lỗi...';
+$lang['Icon size in the menubar'] = 'kích cỡ biểu tượng trên thanh công cụ';
+$lang['Icon size on the identification page'] = 'kích cỡ biểu tượng tại trang nhận dạng';
+$lang['Loading...'] = 'Đang tải...';
+$lang['Logged with'] = 'Đăng nhập với';
+$lang['Or sign in with'] = 'Hoặc đăng ký với';
+$lang['Please enter your OpenID URL'] = 'Vui lòng nhập đường dẫn OpenId của bạn';
+$lang['Associate with an existing account'] = 'Liên kết với tài khoản hiện có';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Một lỗi xảy ra, vui lòng liên hệ chủ Bộ sưu tập. <i>Mã lỗi: %s</i>';
+$lang['Allow users to merge existing account with new <i>Social Connect</i> identity'] = 'Cho phép người dùng nhập tài khoản đã tồn tại với tài khoản <i>mạng xã hội</i> giống nhau';
+$lang['%s: invalid keys'] = '%s: Sai mã';
