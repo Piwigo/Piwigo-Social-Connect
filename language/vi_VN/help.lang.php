95347996056d38574befbc7497e7838b95eb25af
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-12-29 08:50:48 +0100

    Update vi_VN, thanks to dummy

---
+++ language/vi_VN/help.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Fill out any required fields such as the application name and description'] = 'Điền vào các trường được yêu cầu chẳng hạn tên ứng dụng và mô tả';
+$lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Đi tới <a href="%s" target="_blank">%s</a> và tạo một ứng dụng mới';
+$lang['Go to <b>API Parameters</b> and set <em>%s</em> for <b>Redirect URL</b>'] = 'Đi tới <b>Tham số API</b> và đặt <em>%s</em> cho <b>Liên kết chuyển hướng</b>';
+$lang['Go to <b>Settings -> Advanced</b> and set <b>Valid OAuth redirect URIs</b> to <em>%s</em>'] = 'Đi tới <b>Thiết lập -> Nâng cao</b> và đặt <b>OAuth hợp lệ chuyển hướng URI</b> tới <em>%s</em>';
+$lang['Go to <b>Status & Review</b> and set the app public by clicking the big button on top-right'] = 'Đi tới <b>Đánh giá & trạng thái</b> và đặt ứng dụng công khai bằng cách click vào nút lớn ở phía trên bên phải';
+$lang['Based on OpenID'] = 'Dựa trên OpenID';
+$lang['Click on <b>Add Platform</b>, choose <b>Website</b> and set the <b>Site URL</b> to <em>%s</em>'] = 'Click vào <b>Thêm Nền tảng</b>, choọn <b>Website</b> và thiết lập  <b>Liên kết site</b> tới <em>%s</em>';
+$lang['Enter <em>%1s</em> for <b>%2s</b>'] = 'Nhập <em>%1s</em> cho <b>%2s</b>';
+$lang['Go to <b>Settings -> Basic</b> and fill the contact email'] = 'Đi tới <b>Thiết lập -> Cơ bản</b> và điền vào email liên hệ';
+$lang['Go to the <b>Settings</b> tab after creating the app'] = 'Đi tới tab <b>Thiết lập</b> sau khi tạo ứng dụng';
+$lang['No registration required'] = 'Không cần đăng ký';
+$lang['On the <b>APIS & AUTH -> Credentials</b> tab, and click <b>Create new client ID</b>'] = 'Trong tab <b>APIS & AUTH -> Chứng thư</b>, và click <b>Tạo mới ID khách</b>';
+$lang['Once the application is created, click <b>See application details</b>'] = 'Một khi ứng dụng đã được tạo, click <b>Xem chi tiết ứng dụng</b>';
+$lang['Once the process is finished, click on the newly created project'] = 'Một khi tiến trình hoàn tất, click vào dự án mới vừa được tạo';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Một khi bạn đã đăng ký, sao chép và dán chứng thư ứng dụng đã tạo vào trong trang thiết lập';
+$lang['Put your website domain in the <b>%1s</b> field. It must match with the current hostname: <em>%2s</em>'] = 'Đặt tên miền website của bạn vào trường <b>%1s</b>. Nó phải khớp với tên máy chủ hiện tại: <em>%2s</em>';
+$lang['Select these APIs: <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'Chọn các API này: <b>Liên hệ</b> như <em>Đọc</em> và <b>Thư mục xã hội</b> như <em>Đọc công khai</em>';
+$lang['Set <b>%1s</b> to <em>%2s</em>'] = 'Thiết lập <b>%1s</b> vào <em>%2s</em>';
