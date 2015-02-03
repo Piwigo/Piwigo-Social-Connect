fae73e58a0cbd280819b53e857a0879e51e3fe63
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-02-03 13:09:23 +0100

    Update sl_SI, thanks to MarkoAm

---
+++ language/sl_SI/help.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Based on OpenID'] = 'Na podlagi OpenID';
+$lang['Click on <b>Add Platform</b>, choose <b>Website</b> and set the <b>Site URL</b> to <em>%s</em>'] = 'Kliknite na <b>Dodaj Platformo</b>, izberite <b>Spletno stran</b> in nastavite<b>URL naslov strani</b> na <em>%s</em>';
+$lang['Enter <em>%1s</em> for <b>%2s</b>'] = 'Vnesi <em>%1s</em> za <b>%2s</b>';
+$lang['Fill out any required fields such as the application name and description'] = 'Izpolnite vsa zahtevana polja kot so ime in opis aplikacije';
+$lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Pojdite na <a href="%s" target="_blank">%s</a> in kreirajte novo aplikacijo';
+$lang['Go to <b>API Parameters</b> and set <em>%s</em> for <b>Redirect URL</b>'] = 'Pojdite na <b>API Parameteri</b> in nastavite <em>%s</em> za <b>URL preusmeritve</b>';
+$lang['Go to <b>Settings -> Advanced</b> and set <b>Valid OAuth redirect URIs</b> to <em>%s</em>'] = 'Pojdite na <b>Nastavitve -> Napredno</b> in nastavite <b>Veljavne OAuth preusmeritvene URI-je</b> na <em>%s</em>';
+$lang['Go to <b>Settings -> Basic</b> and fill the contact email'] = 'Pojdite na <b>Nastavitve -> Osnovno</b> in izpolnite polje kontaktnega email naslova';
+$lang['Go to <b>Status & Review</b> and set the app public by clicking the big button on top-right'] = 'Pojdite na <b>Stanje & Pregled</b> in nastavite app na javno s klikom na veliki gumb desno zgoraj';
+$lang['Go to the <b>Settings</b> tab after creating the app'] = 'Pojdite na zavihek <b>Nastavitve</b> po kreiranju aplikacije';
+$lang['No registration required'] = 'Registracija ni potrebna';
+$lang['On the <b>APIS & AUTH -> Credentials</b> tab, and click <b>Create new client ID</b>'] = 'Na zavihku <b>APIS & AUTH -> Poverilnice</b>, kliknite na <b>Kreiraj nov ID odjemalca</b>';
+$lang['Once the application is created, click <b>See application details</b>'] = 'Ko je aplikacija kreirana, kliknite na <b>Ogled podrobnosti aplikacije</b>';
+$lang['Once the process is finished, click on the newly created project'] = 'Ko je proces zaključen, kliknite na novo kreiran projekt';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Po registraciji, kopirajte in prilepite poverilnice kreirane aplikacije v to nastavitveno stran';
+$lang['Put your website domain in the <b>%1s</b> field. It must match with the current hostname: <em>%2s</em>'] = 'V polje <b>%1s</b> vpišite domeno vaše spletne strani. Ujemati se mora s trenutnim imenom gostitelja: <em>%2s</em>';
+$lang['Select these APIs: <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'Izberite  API-jevi: <b>Stiki</b> kot <em>Branje</em> in <b>Družabni Direktorij</b> kot <em>Branje Javno</em>';
+$lang['Set <b>%1s</b> to <em>%2s</em>'] = 'Nastavi <b>%1s</b> na <em>%2s</em>';
