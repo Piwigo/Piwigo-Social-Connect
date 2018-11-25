8bf7b9c794a9a2b1f765c2401ef0edda27450658
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-11-25 09:07:18 +0100

    Add ro_RO, thanks to enels

---
+++ language/ro_RO/plugin.lang.php
@@ -0,0 +1,55 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Profile URL'] = 'Adresa URL a profilului';
+$lang['You registered with a %s account, please sign in with the same account.'] = 'Te-ai înregistrat cu un cont %s, te rugăm să te loghezi cu același cont.';
+$lang['Display sign in buttons in the menubar'] = 'Afișați butoanele pentru autentificare în meniu';
+$lang['Sign in with %s'] = 'Autentifică-te cu %s';
+$lang['For security reason, the main webmaster account can\'t be merged with a remote account, but you can use another webmaster account.'] = 'Din motive de securitate, contul principal al webmasterului nu poate fi îmbinat cu un cont la distanță, dar puteți utiliza un alt cont de webmaster.';
+$lang['Disabled'] = 'Dezactivat';
+$lang['Display sign in buttons on the register page'] = 'Afișați butoanele pentru autentificare în meniu';
+$lang['Enabled'] = 'Activat';
+$lang['Please enter your username'] = 'Introduceți numele dvs. de utilizator';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Ne pare rău, noile înregistrări sunt blocate în această galerie.';
+$lang['Your registration is almost done, please complete the registration form.'] = 'Înregistrarea dvs. este aproape finalizată, completați formularul de înregistrare.';
+$lang['Unable to perform login'] = 'Nu se poate efectua logarea';
+$lang['Social Connect: You need to configure the credentials'] = 'Social Connect: Trebuie să configurați acreditările';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'Contactează<b>%s</b>. Așteptați, vă rog.';
+$lang['Or sign in with'] = 'Sau autentifică-te cu';
+$lang['Providers'] = 'Furnizori';
+$lang['Social Connect: PHP Curl extension is needed'] = 'Social Connect: extensia PHP Curl este necesară';
+$lang['User not found'] = 'Utilizatorul nu a fost găsit';
+$lang['Users are not allowed to register on your gallery. Social Connect will not work correctly.'] = 'Utilizatorilor nu li se permite sa se înregistreze în galeria dvs. Social Connect nu va funcționa corect.';
+$lang['Error...'] = 'Eroare...';
+$lang['Please enter your user ID'] = 'Introduceți ID-ul de utilizator';
+$lang['Icon size in the menubar'] = 'Dimensiunea icoanei în bara de meniu';
+$lang['Icon size on the identification page'] = 'Dimensiunea icoanei pe pagina de autentificare';
+$lang['Loading...'] = 'Încarcă...';
+$lang['Logged with'] = 'Autentificat cu';
+$lang['Please enter your OpenID URL'] = 'Introduceți OpenID URL';
+$lang['Cancel'] = 'Anulează';
+$lang['Close'] = 'Închide';
+$lang['Authentication canceled'] = 'Autentificare anulată';
+$lang['Associate with an existing account'] = 'Asociază cu un cont existent';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'A apărut o eroare, va rog sa contactaţi proprietarul galeriei. <i>Cod de eroare: %s</i>';
+$lang['Allow users to merge existing account with new <i>Social Connect</i> identity'] = 'Permite utilizatorilor să îmbine un cont existent cu o nouă <i>Reţea de Socializare</i>';
+$lang['%s: invalid keys'] = '%s chei invalide';
