3ca1b88ca4d75ade1c3a529b78f52f68992f19ce
GunoH <git@guno.nl>
2020-03-07 11:49:22 +0100

    update hybridauth to 2.13

--- include/hybridauth/index.php
+++ include/hybridauth/index.php
@@ -1,18 +1,15 @@
 <?php
-/*!
+/**
 * HybridAuth
 * http://hybridauth.sourceforge.net | http://github.com/hybridauth/hybridauth
-* (c) 2009-2012, HybridAuth authors | http://hybridauth.sourceforge.net/licenses.html 
+* (c) 2009-2015, HybridAuth authors | http://hybridauth.sourceforge.net/licenses.html
 */
 
 // ------------------------------------------------------------------------
 //	HybridAuth End Point
 // ------------------------------------------------------------------------
 
-define('PHPWG_ROOT_PATH', '../../../../');
-include_once(PHPWG_ROOT_PATH.'include/common.inc.php');
-
 require_once( "Hybrid/Auth.php" );
-require_once( "Hybrid/Endpoint.php" ); 
+require_once( "Hybrid/Endpoint.php" );
 
 Hybrid_Endpoint::process();
