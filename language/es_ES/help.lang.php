cc3a78efad009750b5507bc49d53272219e84bca
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-07-15 13:11:11 +0200

    Update es_ES, thanks to Tirs

--- language/es_ES/help.lang.php
+++ language/es_ES/help.lang.php
@@ -21,22 +21,22 @@
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
 $lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Cuando su solicitud sea registrada, copie y pegue las claves de autentificación en esta página';
-$lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Vallan a <a href="%s" target="_blank">%s</a> y crean una nueva aplicación';
+$lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Vaya a <a href="%s" target="_blank">%s</a> y cree una nueva aplicación';
 $lang['Fill out any required fields such as the application name and description'] = 'Rellene todos los campos necesarios como el nombre y la descripción de la aplicación';
 $lang['Based on OpenID'] = 'Basado en OpenID';
-$lang['No registration required'] = 'Identificación no es necesaria';
-$lang['Go to <b>Status & Review</b> and set the app public by clicking the big button on top-right'] = 'Ir a <b> Status & Review </b> y configure el app público haciendo clic en el botón grande en la parte superior derecha';
-$lang['Select these APIs: <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'Selecciona esta APIs: <b>Contacts</b> como <em>Read</em>, and <b>Social Directory</b> como <em>Read Public</em>';
-$lang['Go to <b>Settings -> Basic</b> and fill the contact email'] = 'Ir a <b>Settings -> Basic</b> y rellenar el e-mail de contacto';
-$lang['Go to <b>API Parameters</b> and set <em>%s</em> for <b>Redirect URL</b>'] = 'Ir a <b>API Parameters</b> y ponga <em>%s</em> para <b>Redirect URL</b>';
-$lang['Go to <b>Settings -> Advanced</b> and set <b>Valid OAuth redirect URIs</b> to <em>%s</em>'] = 'Ir a <b>Settings -> Advanced</b> y ponga <b>Valid OAuth redirect URIs</b> en <em>%s</em>';
-$lang['Once the application is created, click <b>See application details</b>'] = 'Una vez creado la aplicación, haga clic en <b>See application details </b>';
+$lang['No registration required'] = 'No es necesaria la identificación';
+$lang['Go to <b>Status & Review</b> and set the app public by clicking the big button on top-right'] = 'Vaya a <b>Status & Review</b> y configure la app pública haciendo clic en el botón grande de la parte superior derecha';
+$lang['Select these APIs: <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'Seleccione esta APIs: <b>Contacts</b> como <em>Read</em>, y <b>Social Directory</b> como <em>Read Public</em>';
+$lang['Go to <b>Settings -> Basic</b> and fill the contact email'] = 'Vaya a <b>Settings -> Basic</b> y rellene el e-mail de contacto';
+$lang['Go to <b>API Parameters</b> and set <em>%s</em> for <b>Redirect URL</b>'] = 'Vaya a <b>API Parameters</b> y ponga <em>%s</em> como <b>Redirect URL</b>';
+$lang['Go to <b>Settings -> Advanced</b> and set <b>Valid OAuth redirect URIs</b> to <em>%s</em>'] = 'Vaya a <b>Settings -> Advanced</b> y ponga <b>Valid OAuth redirect URIs</b> en <em>%s</em>';
+$lang['Once the application is created, click <b>See application details</b>'] = 'Una vez creada la aplicación, haga clic en <b>See application details </b>';
 $lang['Set <b>%1s</b> to <em>%2s</em>'] = 'Ponga <b>%1s</b> en <em>%2s</em>';
-$lang['Put your website domain in the <b>%1s</b> field. It must match with the current hostname: <em>%2s</em>'] = 'Ponga su dominio del sitio web en el campo <b>% 1s </ b>. Debe coincidir con el nombre de host actual: <em>% 2s </ em>';
-$lang['Once the process is finished, click on the newly created project'] = 'Una vez el proceso terminado, haga clic en el recién creado proyecto';
+$lang['Put your website domain in the <b>%1s</b> field. It must match with the current hostname: <em>%2s</em>'] = 'Ponga su dominio del sitio web en el campo <b>%1s</b>. Debe coincidir con el nombre de host actual: <em>%2s</em>';
+$lang['Once the process is finished, click on the newly created project'] = 'Una vez terminado el proceso, haga clic en el proyecto recién creado';
 $lang['Enter <em>%1s</em> for <b>%2s</b>'] = 'Ponga <em>%1s</em> para <b>%2s</b>';
-$lang['Click on <b>Add Platform</b>, choose <b>Website</b> and set the <b>Site URL</b> to <em>%s</em>'] = 'hacer clic en <b>Add Platform</b>, eligir <b>Website</b> y ponga el <b>Site URL</b> en <em>%s</em>';
-$lang['Go to the <b>Settings</b> tab after creating the app'] = 'Ir a <b> Configuración </b> después de la creación de la aplicación';
+$lang['Click on <b>Add Platform</b>, choose <b>Website</b> and set the <b>Site URL</b> to <em>%s</em>'] = 'Haga clic en <b>Add Platform</b>, elija <b>Website</b> y ponga el <b>Site URL</b> en <em>%s</em>';
+$lang['Go to the <b>Settings</b> tab after creating the app'] = 'Vaya a <b>Configuración</b> después de la creación de la aplicación';
 $lang['Click on <b>Configure consent screen</b>, fill the <b>Product name</b> and save'] = 'Haga clic en <b>Configurar pantalla de consentimiento</b>, complete el <b>Nombre del producto</b> y guarde';
 $lang['Go to the <b>Library</b> tab, open <b>Social APIs -> Google+ API</b> and click <b>ENABLE</b>'] = 'Vaya a la pestaña <b>Biblioteca</b>, abra <b>API sociales -> API de Google+</b> y haga clic en <b>HABILITAR</b>';
 $lang['On the <b>Credentials</b> tab, click <b>Create credentials -> OAuth client ID</b>'] = 'En la pestaña <b>Credenciales</b>, haga clic en <b>Crear credenciales -> ID de cliente OAuth</b>';
