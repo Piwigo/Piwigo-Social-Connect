2c07e588071c4e0b6441c6b0a0f85ff668aa31c8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-24 07:51:34 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/help.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Based on OpenID'] = 'Byggt á OpenID';
+$lang['Click on <b>Add Platform</b>, choose <b>Website</b> and set the <b>Site URL</b> to <em>%s</em>'] = 'Smelltu á <b>Bæta við vettvangi</b>, veldu <b>Vefsíðu</b> og stilltu <b>Vefslóðina</b> á <em>%s</em>';
+$lang['Click on <b>Configure consent screen</b>, fill the <b>Product name</b> and save'] = 'Smelltu á <b>Stilla samþykkisskjá</b>, fylltu út <b>Vöruheiti</b> og vistaðu';
+$lang['Enter <em>%1s</em> for <b>%2s</b>'] = 'Sláðu inn <em>%1s</em> fyrir <b>%2s</b>';
+$lang['Fill out any required fields such as the application name and description'] = 'Fylltu út alla nauðsynlega reiti eins og heiti forrits og lýsingu';
+$lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Farðu í <a href="%s" target="_blank">%s</a> og stofnaðu nýtt forrit';
+$lang['Go to <b>API Parameters</b> and set <em>%s</em> for <b>Redirect URL</b>'] = 'Farðu í <b>API færibreytur</b> og stilltu <em>%s</em> fyrir <b>Tilvísunar vefslóð</b>';
+$lang['Go to <b>Settings -> Advanced</b> and set <b>Valid OAuth redirect URIs</b> to <em>%s</em>'] = 'Farðu í <b>Stillingar -> Ítarlegar</b> og stilltu <b>Gildar OAuth tilvísunar vefslóðir</b> á <em>%s</em>';
+$lang['Go to <b>Settings -> Basic</b> and fill the contact email'] = 'Farðu í <b>Stillingar -> Grunnur</b> og fylltu út tengiliðanetfangið';
+$lang['Go to <b>Status & Review</b> and set the app public by clicking the big button on top-right'] = 'Farðu í <b>Staða og skoðun</b> og stilltu forritið í opinbert með því að smella á stóra hnappinn efst til hægri';
+$lang['Go to the <b>Library</b> tab, open <b>Social APIs -> Google+ API</b> and click <b>ENABLE</b>'] = 'Farðu í flipann <b>Safn</b>, opnaðu <b>Samfélags APIs -> Google+ API</b> og smelltu á <b>VIRKJA</b>';
+$lang['Go to the <b>Settings</b> tab after creating the app'] = 'Farðu í flipann <b>Stillingar</b> eftir að forritið hefur verið stofnað';
+$lang['No registration required'] = 'Engrar skráningar krafist';
+$lang['On the <b>Credentials</b> tab, click <b>Create credentials -> OAuth client ID</b>'] = 'Í flipanum <b>Skilríki</b> skaltu smella á <b>Búa til skilríki -> Auðkenni OAuth biðlara</b>';
+$lang['Once the application is created, click <b>See application details</b>'] = 'Þegar forritið hefur verið stofnað skaltu smella á <b>Sjá forritaupplýsingar</b>';
+$lang['Once the process is finished, click on the newly created project'] = 'Þegar ferlinu er lokið skaltu smella á nýstofnaða verkefnið';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Þegar þú hefur skráð þig, afritaðu og límdu stofnað forritaskilríki inn á þessa uppsetningarsíðu';
+$lang['Put your website domain in the <b>%1s</b> field. It must match with the current hostname: <em>%2s</em>'] = 'Settu vefsvæðalénið þitt í <b>%1s</b> reitinn. Það verður að passa við núverandi hýsingarheiti: <em>%2s</em>';
+$lang['Select these APIs: <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'Veldu þessi API: <b>Tengiliðir</b> sem <em>Lesa</em> og <b>Samfélagsskrá</b> sem <em>Lesa opinbert</em>';
+$lang['Set <b>%1s</b> to <em>%2s</em>'] = 'Stilltu <b>%1s</b> á <em>%2s</em>';
---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Allow users to merge existing account with new <i>Social Connect</i> identity'] = 'Leyfa notendum að sameina núverandi reikning við nýtt auðkenni <i>Samfélagstengingar</i>';
+$lang['Please enter your user ID'] = 'Vinsamlega sláðu inn notendaauðkenni þitt';
+$lang['Please enter your username'] = 'Vinsamlegast sláðu inn notendanafnið þitt';
+$lang['Profile URL'] = 'Vefslóð prófíls';
+$lang['Providers'] = 'Veitendur';
+$lang['Sign in with %s'] = 'Skráðu þig inn með %s';
+$lang['Social Connect: PHP Curl extension is needed'] = 'Samfélagstenging: PHP Curl viðbót er nauðsynleg';
+$lang['Social Connect: You need to configure the credentials'] = 'Samfélagstenging: Þú þarft að stilla skilríkin';
+$lang['Sorry, new registrations are blocked on this gallery.'] = 'Því miður er lokað fyrir nýjar skráningar í þetta gallerí.';
+$lang['Unable to perform login'] = 'Ekki hægt að framkvæma innskráningu';
+$lang['User not found'] = 'Notandi fannst ekki';
+$lang['Users are not allowed to register on your gallery. Social Connect will not work correctly.'] = 'Notendum er ekki heimilt að skrá sig inn í galleríið þitt. Samfélagstenging mun ekki virka rétt.';
+$lang['You registered with a %s account, please sign in with the same account.'] = 'Þú skráðir þig með %s reikningi, vinsamlegast skráðu þig inn með sama reikningi.';
+$lang['Your registration is almost done, please complete the registration form.'] = 'Skráningu þinni er næstum lokið, vinsamlegast fylltu út skráningareyðublaðið.';
+$lang['%s: invalid keys'] = '%s: ógildir lyklar';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = 'Villa kom upp, vinsamlegast hafðu samband við eiganda gallerísins. <i>Villukóði: %s</i>';
+$lang['Associate with an existing account'] = 'Tengja við núverandi reikning';
+$lang['Authentication canceled'] = 'Hætt við auðkenningu';
+$lang['Cancel'] = 'Hætta við';
+$lang['Close'] = 'Loka';
+$lang['Contacting <b>%s</b>. Please wait.'] = 'Hef samband við <b>%s</b>. Vinsamlegast bíðið.';
+$lang['Disabled'] = 'Óvirkjað';
+$lang['Display sign in buttons in the menubar'] = 'Birta innskráningarhnappa á valmyndastikunni';
+$lang['Display sign in buttons on the register page'] = 'Birta innskráningarhnappa á skráningarsíðunni';
+$lang['Enabled'] = 'Virkjað';
+$lang['Error...'] = 'Villur...';
+$lang['For security reason, the main webmaster account can\'t be merged with a remote account, but you can use another webmaster account.'] = 'Af öryggisástæðum er ekki hægt að sameina aðalnetstjórareikninginn við ytri reikning, en þú getur notað annan vefstjórareikning.';
+$lang['Icon size in the menubar'] = 'Táknstærð í valmyndastikunni';
+$lang['Icon size on the identification page'] = 'Táknstærð á auðkennissíðunni';
+$lang['Loading...'] = 'Hleð upp...';
+$lang['Logged with'] = 'Skráður með';
+$lang['Or sign in with'] = 'Eða skráðu þig inn með';
+$lang['Please enter your OpenID URL'] = 'Vinsamlegast sláðu inn OpenID vefslóðina þína';
