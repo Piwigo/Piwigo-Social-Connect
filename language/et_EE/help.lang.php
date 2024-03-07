ea6895e0f465b9727ab435fbe01b0932600279c0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-03-07 18:03:22 +0100

    Update et_EE, thanks to Exabot

--- language/et_EE/help.lang.php
+++ language/et_EE/help.lang.php
@@ -20,14 +20,9 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['Set <b>%s</b> to <em>%s</em>'] = 'Seadista <b>%s</b> järgmiseks <em>%s</em>';
-$lang['Provide this URL as the Callback/Redirect URL for your application: <em>%s</em>'] = 'Paku seda URL\'i ümbersuunamiseks sinu aplikatsioonile: <em>%s</em>';
-$lang['Put your website domain in the %s fields. It should match with the current hostname: <em>%s</em>'] = 'Pane oma veebilehe domeen %s väljale. See peab olema sama mis praegune hosti nimi: <em>%s</em>';
-$lang['Once the application is registered update the permissions : set <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = 'Kui aplikatsioon on registreeritud uuenda õigusi : sea <b>Contacts</b> kui <em>Read</em> ja <b>Social Directory</b> kui <em>Read Public</em>';
 $lang['Once you have registered, copy and past the created application credentials into this setup page'] = 'Pärast registreerimist kopeeri loodud aplikatsiooni info seadistuse lehele';
 $lang['Fill out any required fields such as the application name and description'] = 'Täida kõik vajalikud väljad nagu nimi ja kirjeldus';
 $lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = 'Mine <a href="%s" target="_blank">%s</a> ja loo uus aplikatsioon';
 $lang['Based on OpenID'] = 'Põhineb OpenID\'l';
-$lang['Go to <b>Settings->Advanced</b> and activate <em>Client OAuth Login</em>'] = 'Mine <b>Seaded->Edasijõudnutele</b> ja aktiveeri <em>Client OAuth Login</em>';
 $lang['No registration required'] = 'Registreerimine ei ole kohustuslik';
-$lang['On the <b>APIs & auth -> Credentials</b> tab, <b>Create new client ID</b>'] = '<b>APIs & autoriseering -> Credentials</b> lehel valida, <b>Loo uue kliendi ID</b>';
+$lang['Go to <b>Settings -> Basic</b> and fill the contact email'] = 'Avage <b>Seaded -> Põhiline</b> ja täitke kontakti e-posti aadress';
