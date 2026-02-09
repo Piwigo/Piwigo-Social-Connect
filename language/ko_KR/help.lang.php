6e999a0bc801d204f2a9b7a2fba50d646b82ca1b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-09 08:21:17 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/help.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Based on OpenID'] = 'OpenID 기반';
+$lang['Click on <b>Add Platform</b>, choose <b>Website</b> and set the <b>Site URL</b> to <em>%s</em>'] = '<b>플랫폼 추가</b>를 클릭하고 <b>웹사이트</b>를 선택한 후 <b>사이트 URL</b>을 <em>%s</em>로 설정하세요';
+$lang['Click on <b>Configure consent screen</b>, fill the <b>Product name</b> and save'] = '<b>동의 화면 구성</b>을 클릭하고 <b>제품 이름</b>을 입력한 후 저장하세요';
+$lang['Enter <em>%1s</em> for <b>%2s</b>'] = '<em>%1s</em>를 <b>%2s</b>에 입력하세요';
+$lang['Fill out any required fields such as the application name and description'] = '애플리케이션 이름 및 설명 등 필수 항목을 입력하세요';
+$lang['Go to <a href="%s" target="_blank">%s</a> and create a new application'] = '<a href="%s" target="_blank">%s</a>로 이동하여 새 애플리케이션을 생성하세요';
+$lang['Go to <b>API Parameters</b> and set <em>%s</em> for <b>Redirect URL</b>'] = '<b>API 매개변수</b>로 이동하여 <b>리다이렉트 URL</b>에 <em>%s</em>를 설정하세요';
+$lang['Go to <b>Settings -> Advanced</b> and set <b>Valid OAuth redirect URIs</b> to <em>%s</em>'] = '<b>설정 -> 고급</b>으로 이동하여 <b>유효한 OAuth 리디렉션 URI</b>를 <em>%s</em>로 설정하세요';
+$lang['Go to <b>Settings -> Basic</b> and fill the contact email'] = '<b>설정 -> 기본</b>으로 이동하여 연락처 이메일을 입력하세요';
+$lang['Go to <b>Status & Review</b> and set the app public by clicking the big button on top-right'] = '<b>상태 및 검토</b>로 이동하여 오른쪽 상단의 큰 버튼을 클릭하여 앱 공개 설정';
+$lang['Go to the <b>Library</b> tab, open <b>Social APIs -> Google+ API</b> and click <b>ENABLE</b>'] = '<b>라이브러리</b> 탭으로 이동하여 <b>소셜 API -> Google+ API</b>를 열고 <b>활성화</b>를 클릭하세요';
+$lang['Go to the <b>Settings</b> tab after creating the app'] = '앱 생성 후 <b>설정</b> 탭으로 이동하세요';
+$lang['No registration required'] = '등록 불필요';
+$lang['On the <b>Credentials</b> tab, click <b>Create credentials -> OAuth client ID</b>'] = '<b>인증 정보</b> 탭에서 <b>인증 정보 생성 -> OAuth 클라이언트 ID</b>를 클릭하세요';
+$lang['Once the application is created, click <b>See application details</b>'] = '애플리케이션 생성 후 <b>애플리케이션 세부정보 보기</b>를 클릭하세요';
+$lang['Once the process is finished, click on the newly created project'] = '프로세스가 완료되면 새로 생성된 프로젝트를 클릭하세요';
+$lang['Once you have registered, copy and past the created application credentials into this setup page'] = '등록 완료 후 생성된 애플리케이션 자격 증명을 이 설정 페이지에 복사하여 붙여넣으세요';
+$lang['Put your website domain in the <b>%1s</b> field. It must match with the current hostname: <em>%2s</em>'] = '웹사이트 도메인을 <b>%1s</b> 필드에 입력하세요. 현재 호스트명 <em>%2s</em>와 반드시 일치해야 합니다';
+$lang['Select these APIs: <b>Contacts</b> as <em>Read</em> and <b>Social Directory</b> as <em>Read Public</em>'] = '다음 API를 선택하세요: <b>연락처</b>는 <em>읽기</em>로, <b>소셜 디렉토리</b>는 <em>공개 읽기</em>로 설정하세요';
+$lang['Set <b>%1s</b> to <em>%2s</em>'] = '<b>%1s</b>를 <em>%2s</em>로 설정하세요';
---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Please enter your user ID'] = '사용자 ID를 입력하세요';
+$lang['Please enter your username'] = '사용자 이름을 입력하세요';
+$lang['Profile URL'] = '프로필 URL';
+$lang['Providers'] = '서비스 제공자';
+$lang['Sign in with %s'] = '%s로 로그인하세요';
+$lang['Social Connect: PHP Curl extension is needed'] = 'Social Connect: PHP Curl 확장 모듈이 필요합니다';
+$lang['Social Connect: You need to configure the credentials'] = 'Social Connect: 인증 정보를 구성해야 합니다';
+$lang['Sorry, new registrations are blocked on this gallery.'] = '죄송합니다. 이 갤러리에서는 신규 등록이 차단되었습니다.';
+$lang['Unable to perform login'] = '로그인할 수 없습니다';
+$lang['User not found'] = '사용자를 찾을 수 없습니다';
+$lang['Users are not allowed to register on your gallery. Social Connect will not work correctly.'] = '사용자는 귀하의 갤러리에 등록할 수 없습니다. Social Connect가 정상적으로 작동하지 않습니다.';
+$lang['You registered with a %s account, please sign in with the same account.'] = '%s 계정으로 등록하셨습니다. 동일한 계정으로 로그인해 주세요.';
+$lang['Your registration is almost done, please complete the registration form.'] = '등록이 거의 완료되었습니다. 등록 양식을 완료해 주세요.';
+$lang['%s: invalid keys'] = '%s: 유효하지 않은 키';
+$lang['Allow users to merge existing account with new <i>Social Connect</i> identity'] = '사용자가 기존 계정을 새로운 <i>Social Connect</i> ID와 병합할 수 있도록 허용';
+$lang['An error occured, please contact the gallery owner. <i>Error code : %s</i>'] = '오류가 발생했습니다. 갤러리 소유자에게 문의하십시오. <i>Error code : %s</i>';
+$lang['Associate with an existing account'] = '기존 계정과 연결';
+$lang['Authentication canceled'] = '인증 취소됨';
+$lang['Cancel'] = '취소';
+$lang['Close'] = '닫기';
+$lang['Contacting <b>%s</b>. Please wait.'] = '<b>%s</b>에 연결 중입니다. 잠시만 기다려 주세요.';
+$lang['Disabled'] = '사용 안 함';
+$lang['Display sign in buttons in the menubar'] = '메뉴바에 로그인 버튼 표시';
+$lang['Display sign in buttons on the register page'] = '등록 페이지에 로그인 버튼 표시';
+$lang['Enabled'] = '사용 함';
+$lang['Error...'] = '오류 발생...';
+$lang['For security reason, the main webmaster account can\'t be merged with a remote account, but you can use another webmaster account.'] = '보안상의 이유로 메인 웹마스터 계정은 원격 계정과 병합할 수 없지만, 다른 웹마스터 계정을 사용할 수 있습니다.';
+$lang['Icon size in the menubar'] = '메뉴바 아이콘 크기';
+$lang['Icon size on the identification page'] = '인증 페이지 아이콘 크기';
+$lang['Loading...'] = '로드 중...';
+$lang['Logged with'] = '로그인됨';
+$lang['Or sign in with'] = '또는 다음으로 로그인';
+$lang['Please enter your OpenID URL'] = 'OpenID URL을 입력하세요';
